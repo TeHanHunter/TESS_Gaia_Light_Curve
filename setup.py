1765b33f9d771635013e077c3e8d64783441c4de
Te Han <tehanhunter@gmail.com>
2021-11-30 15:40:31 -0800

    lc background correction

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 import sys
 import setuptools
-from SEBIT.version import __version__
+from TGLC.version import __version__
 
-sys.path.insert(0, "SEBIT")
+sys.path.insert(0, "TGLC")
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name="SEBIT",
+    name="TGLC",
     version=__version__,
     author="Te Han",
     author_email="tehanhunter@gmail.com",
-    description="Searching Eclipsing Binaries in TESS",
+    description="TESS-Gaia Light Curve",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TeHanHunter/Searching-Eclipsing-Binaries-in-TESS",
+    url="https://github.com/TeHanHunter/TESS_Gaia_Light_Curve",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=setuptools.find_packages(include=['SEBIT', 'SEBIT.*']),
+    packages=setuptools.find_packages(include=['TGLC', 'TGLC.*']),
     python_requires=">=3.6",
 )
