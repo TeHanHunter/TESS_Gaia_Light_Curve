61672fe4a4720cedd449c9ee647bd8ef0f20f283
Te Han <tehanhunter@gmail.com>
2022-05-27 13:51:59 -0700

    change package name to tglc

---
+++ build/lib/tglc/effective_psf.py
@@ -0,0 +1,245 @@
+import numpy as np
+
+
+def bilinear(x, y, repeat=23):
+    '''
+    A bilinear formula
+    np.array([1 - x - y + x * y, x - x * y, y - x * y, x * y] * repeat)
+    b, d = array[1]
+    a, c = array[0]
+    :param x: x
+    :param y: y
+    :param repeat: side length of epsf
+    :return: bilinear interpolation
+    '''
+    return np.array([1 - x - y + x * y, x - x * y, y - x * y, x * y] * repeat)
+
+
+def get_psf(source, factor=2, psf_size=11, edge_compression=1e-4, c=np.array([0, 0, 0])):
+    """
+    Generate matrix for PSF fitting
+    :param source: tglc.ffi.Source or tglc.ffi_cut.Source_cut, required
+    Source or Source_cut object
+    :param factor: int, optional
+    effective PSF oversampling factor
+    :param psf_size: int, optional
+    effective PSF side length
+    :param edge_compression: float, optional
+    parameter for edge compression
+    :param c: np.ndarray, optional
+    manual modification of Gaia positions in the format of [x, y, theta]
+    :return: A, star_info, over_size, x_round, y_round
+    A: 2d matrix for least_square
+    star_info: star parameters
+    over_size: size of oversampled grid of ePSF
+    x_round: star horizontal pixel coordinates rounded
+    y_round: star vertical pixel coordinates rounded
+    """
+    # even only
+    if factor % 2 != 0:
+        raise ValueError('Factor must be even.')
+    psf_size = psf_size
+    half_size = int((psf_size - 1) / 2)
+    over_size = psf_size * factor + 1
+    size = source.size  # TODO: must be even?
+    flux_ratio = np.array(source.gaia['tess_flux_ratio'])
+    # flux_ratio = 0.9998 * flux_ratio + 0.0002
+    # x_shift = np.array(source.gaia[f'sector_{source.sector}_x'])
+    # y_shift = np.array(source.gaia[f'sector_{source.sector}_y'])
+
+    x_ = np.array(source.gaia[f'sector_{source.sector}_x'])
+    y_ = np.array(source.gaia[f'sector_{source.sector}_y'])
+
+    x_shift = (x_ - c[0]) * np.cos(c[2]) - (y_ - c[1]) * np.sin(c[2])
+    y_shift = (x_ - c[0]) * np.sin(c[2]) + (y_ - c[1]) * np.cos(c[2])
+
+    x_round = np.round(x_shift).astype(int)
+    y_round = np.round(y_shift).astype(int)
+
+    left = np.maximum(0, x_round - half_size)
+    right = np.minimum(size, x_round + half_size) + 1
+    down = np.maximum(0, y_round - half_size)
+    up = np.minimum(size, y_round + half_size) + 1
+    x_residual = x_shift % (1 / factor) * factor
+    y_residual = y_shift % (1 / factor) * factor
+
+    x_p = np.arange(size)
+    y_p = np.arange(size)
+    coord = np.arange(size ** 2).reshape(size, size)
+    A = np.zeros((size ** 2, over_size ** 2 + 3))
+    xx, yy = np.meshgrid((np.arange(size) - (size - 1) / 2), (np.arange(size) - (size - 1) / 2))
+    A[:, -1] = np.ones(size ** 2)
+    A[:, -2] = yy.flatten()
+    A[:, -3] = xx.flatten()
+    star_info = []
+    for i in range(len(source.gaia)):
+        x_psf = factor * (x_p[left[i]:right[i]] - x_round[i] + half_size) + (x_shift[i] % 1) // (1 / factor)
+        y_psf = factor * (y_p[down[i]:up[i]] - y_round[i] + half_size) + (y_shift[i] % 1) // (1 / factor)
+        x_psf, y_psf = np.meshgrid(x_psf, y_psf)  # super slow here
+        a = np.array(x_psf + y_psf * over_size, dtype=np.int64).flatten()
+        index = coord[down[i]:up[i], left[i]:right[i]]
+        A[np.repeat(index, 4), np.array([a, a + 1, a + over_size, a + over_size + 1]).flatten(order='F')] += \
+            flux_ratio[i] * bilinear(x_residual[i], y_residual[i], repeat=len(a))
+        star_info.append(
+            (np.repeat(index, 4), np.array([a, a + 1, a + over_size, a + over_size + 1]).flatten(order='F'),
+             flux_ratio[i] * bilinear(x_residual[i], y_residual[i], repeat=len(a))))
+
+    coord_ = np.arange(- psf_size * factor / 2 + 1, psf_size * factor / 2 + 2)
+    x_coord, y_coord = np.meshgrid(coord_, coord_)
+    variance = psf_size
+    dist = (1 - np.exp(- 0.5 * (x_coord ** 4 + y_coord ** 4) / variance ** 4)) * edge_compression  # 1e-3
+    A_mod = np.diag(dist.flatten())
+    A_mod = np.concatenate((A_mod, (np.zeros((over_size ** 2, 3)))), axis=-1)
+    A = np.append(A, A_mod, axis=0)
+    return A, star_info, over_size, x_round, y_round
+
+
+def fit_psf(A, source, over_size, power=0.8, time=0):
+    """
+    fit_psf using least_square (improved performance by changing to np.linalg.solve)
+    :param A: np.ndarray, required
+    2d matrix for least_square
+    :param source: tglc.ffi.Source or tglc.ffi_cut.Source_cut, required
+    Source or Source_cut object
+    :param over_size: int, required
+    size of oversampled grid of ePSF
+    :param power: float, optional
+    power for weighting bright stars' contribution to the fit. 1 means same contribution from all stars,
+    <1 means emphasizing dimmer stars
+    :param time: int, required
+    time index of this ePSF fit
+    :return: fit result
+    """
+    b = source.flux[time].flatten()
+    b = np.append(b, np.zeros(over_size ** 2))
+    scaler = (source.flux[time].flatten()) ** power  # source.flux_err[time].flatten() ** 2 +
+    scaler = np.append(scaler, np.ones(over_size ** 2))
+
+    # fit = np.linalg.lstsq(A / scaler[:, np.newaxis], b / scaler, rcond=None)[0]
+    a = A / scaler[:, np.newaxis]
+    b = b / scaler
+    alpha = np.dot(a.T, a)
+    beta = np.dot(a.T, b)
+    fit = np.linalg.solve(alpha, beta)
+    return fit
+
+
+def fit_lc(A, source, star_info=None, x=0., y=0., star_num=0, factor=2, psf_size=11, e_psf=None, near_edge=False):
+    """
+    Produce matrix for least_square fitting without a certain target
+    :param A: np.ndarray, required
+    2d matrix for least_square
+    :param source: tglc.ffi.Source or tglc.ffi_cut.Source_cut, required
+    Source or Source_cut object
+    :param star_info: np.ndarray, required
+    star parameters
+    :param x: float, required
+    target horizontal pixel coordinate
+    :param y: float, required
+    target vertical pixel coordinate
+    :param star_num: int, required
+    target star index
+    :param factor: int, optional
+    effective PSF oversampling factor
+    :param psf_size: int, optional
+    effective PSF side length
+    :param e_psf: np.ndarray, required
+    effective PSF as a 3d array as a timeseries
+    :param near_edge: boolean, required
+    whether the star is 2 pixels or closer to the edge of a CCD
+    :return: aperture lightcurve, PSF lightcurve, vertical pixel coord, horizontal pixel coord, portion of light in aperture
+    """
+    size = source.size  # TODO: must be even?
+    star_position = int(x + source.size * y - 5 * size - 5)
+    # aper_lc
+    left = np.maximum(0, x - 2)
+    right = np.minimum(size, x + 2) + 1
+    down = np.maximum(0, y - 2)
+    up = np.minimum(size, y + 2) + 1
+    coord = np.arange(size ** 2).reshape(size, size)
+    index = np.array(coord[down:up, left:right]).flatten()
+    A_cut = np.zeros((len(index), np.shape(A)[1]))
+    for i in range(len(index)):
+        A_ = np.zeros(np.shape(A)[-1])
+        star_pos = np.where(star_info[star_num][0] == index[i])[0]
+        A_[star_info[star_num][1][star_pos]] = star_info[star_num][2][star_pos]
+        A_cut[i] = A[index[i], :] - A_
+    aperture_lc = np.zeros((len(index), len(source.time)))
+    for j in range(len(source.time)):
+        aperture_lc[:, j] = np.array(source.flux[j][down:up, left:right]).flatten() - np.dot(A_cut, e_psf[j])
+    aperture_lc = aperture_lc.reshape((up - down, right - left, len(source.time)))
+
+    # psf_lc
+    over_size = psf_size * factor + 1
+    if near_edge:
+        psf_lc = np.zeros(len(source.time))
+        psf_lc[:] = np.NaN
+        e_psf_1d = np.nanmedian(e_psf[:, :over_size ** 2], axis=0).reshape(over_size, over_size)
+        portion = (36 / 49) * np.nansum(e_psf_1d[8:15, 8:15]) / np.nansum(e_psf_1d)  # only valid for factor = 2
+        return aperture_lc, psf_lc, y - down, x - left, portion
+    left_ = left - x + 5
+    right_ = right - x + 5
+    down_ = down - y + 5
+    up_ = up - y + 5
+
+    left_11 = np.maximum(- x + 5, 0)
+    right_11 = np.minimum(size - x + 5, 11)
+    down_11 = np.maximum(- y + 5, 0)
+    up_11 = np.minimum(size - y + 5, 11)
+    coord = np.arange(psf_size ** 2).reshape(psf_size, psf_size)
+    index = coord[down_11:up_11, left_11:right_11]
+
+    A = np.zeros((psf_size ** 2, over_size ** 2 + 3))
+    A[np.repeat(index, 4), star_info[star_num][1]] = star_info[star_num][2]
+    psf_shape = np.dot(A, e_psf.T).reshape(psf_size, psf_size, len(source.time))
+    psf_sim = psf_shape[down_:up_, left_: right_, :]
+    # f, (ax1, ax2) = plt.subplots(1, 2)
+    # ax1.imshow(aperture_lc[:, :, 0])
+    # ax2.imshow(psf_shape[:, :, 0])
+    # plt.show()
+    psf_lc = np.zeros(len(source.time))
+    for j in range(len(source.time)):
+        if np.isnan(psf_sim[:, :, j]).any():
+            psf_lc[j] = np.nan
+        else:
+            psf_lc[j] = np.linalg.lstsq(np.reshape(psf_sim[:, :, j].flatten(), (25, 1)) / np.nansum(psf_sim[:, :, j]),
+                                        aperture_lc[:, :, j].flatten())[0]
+    portion = np.nansum(psf_shape[4:7, 4:7, :]) / np.nansum(psf_shape)
+    return aperture_lc, psf_lc, y - down, x - left, portion
+
+
+def bg_mod(source, q=None, aper_lc=None, psf_lc=None, portion=None, star_num=0, near_edge=False):
+    '''
+    background modification
+    :param source: tglc.ffi.Source or tglc.ffi_cut.Source_cut, required
+    Source or Source_cut object
+    :param q: list, optional
+    list of booleans that filter the data points
+    :param aper_lc: np.ndarray, required
+    aperture light curve
+    :param psf_lc: np.ndarray, required
+    PSF light curve
+    :param portion: float, required
+    portion of light in aperture
+    :param star_num: int, required,
+    star index
+    :param near_edge: boolean, required
+    whether the star is 2 pixels or closer to the edge of a CCD
+    :return: local background, modified aperture light curve, modified PSF light curve
+    '''
+    bar = 15000 * 10 ** ((source.gaia['tess_mag'][star_num] - 10) / -2.5)
+    # med_epsf = np.nanmedian(e_psf[:, :23 ** 2].reshape(len(source.time), 23, 23), axis=0)
+    # centroid_to_aper_ratio = 4/9 * np.sum(med_epsf[10:13, 10:13]) / np.sum(med_epsf)
+    # centroid_to_aper_ratio = np.nanmedian(ratio)
+    # flux_bar = aperture_bar * centroid_to_aper_ratio
+    # lightcurve = lightcurve + (flux_bar - np.nanmedian(lightcurve[q]))
+    aperture_bar = bar * portion
+    local_bg = np.nanmedian(aper_lc[q]) - aperture_bar
+    aper_lc = aper_lc - local_bg
+    if near_edge:
+        return local_bg, aper_lc, psf_lc
+    # print(local_bg / aperture_bar)
+    psf_bar = bar
+    local_bg = np.nanmedian(psf_lc[q]) - psf_bar
+    psf_lc = psf_lc - local_bg
+    return local_bg, aper_lc, psf_lc
---
+++ build/lib/tglc/ffi_cut.py
@@ -0,0 +1,164 @@
+import os
+import sys
+import warnings
+
+import numpy as np
+from astropy.coordinates import SkyCoord
+from astropy.table import Table, hstack
+from astropy.wcs import WCS
+from astroquery.mast import Catalogs
+from astroquery.mast import Tesscut
+import pickle
+from os.path import exists
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+class Source_cut(object):
+    def __init__(self, name, size=15, sector=None, cadence=None):
+        """
+        Source_cut object that includes all data from TESS and Gaia DR2
+        :param name: str, required
+        Target identifier (e.g. "NGC 7654" or "M31"),
+        or coordinate in the format of ra dec (e.g. '351.40691 61.646657')
+        :param size: int, optional
+        The side length in pixel  of TESScut image
+        :param cadence: list, required
+        list of cadences of TESS FFI
+        """
+        super(Source_cut, self).__init__()
+        if cadence is None:
+            cadence = []
+        self.name = name
+        self.size = size
+        self.sector = 0
+        self.wcs = []
+        self.time = []
+        self.flux = []
+        self.flux_err = []
+        self.gaia = []
+        self.cadence = cadence
+        self.quality = []
+        catalogdata = Catalogs.query_object(self.name, radius=(self.size + 4) * 21 * 0.707 / 3600,
+                                            catalog="Gaia", version=2)
+        print(f'Target Gaia: {catalogdata[0]["designation"]}')
+        # TODO: maybe increase search radius
+        print(f'Found {len(catalogdata)} Gaia DR2 objects.')
+        ra = catalogdata[0]['ra']
+        dec = catalogdata[0]['dec']
+        coord = SkyCoord(ra, dec, unit="deg")
+        catalogdata_tic = Catalogs.query_object(coord.to_string(), radius=(self.size + 4) * 21 * 0.707 / 3600,
+                                            catalog="TIC")
+        print(f'Found {len(catalogdata_tic)} TIC objects.')
+        self.tic = catalogdata_tic['ID', 'GAIA']
+        sector_table = Tesscut.get_sectors(coord)
+        print(sector_table)
+        if sector is None:
+            hdulist = Tesscut.get_cutouts(coord, self.size)
+        else:
+            hdulist = Tesscut.get_cutouts(coord, self.size, sector=sector)
+        self.catalogdata = catalogdata
+        self.sector_table = sector_table
+        self.camera = int(sector_table[0]['camera'])
+        self.ccd = int(sector_table[0]['ccd'])
+        self.hdulist = hdulist
+        sector_list = []
+        for i in range(len(hdulist)):
+            sector_list.append(hdulist[i][0].header['SECTOR'])
+        self.sector_list = sector_list
+        if sector is None:
+            self.select_sector(sector=sector_table['sector'][0])
+        else:
+            self.select_sector(sector=sector)
+
+
+    def select_sector(self, sector=1):
+        """
+        select sector to use if target is in multi-sectors
+        :param sector: int, required
+        TESS sector number
+        """
+        if self.sector == sector:
+            print(f'Already in sector {sector}.')
+            return
+        elif sector not in self.sector_table['sector']:
+            print(f'Sector {sector} does not cover this region. Please refer to sector table.')
+            return
+
+        index = self.sector_list.index(sector)
+        self.sector = sector
+        hdu = self.hdulist[index]
+        self.camera = int(hdu[0].header['CAMERA'])
+        self.ccd = int(hdu[0].header['CCD'])
+        wcs = WCS(hdu[2].header)
+        data_time = hdu[1].data['TIME']
+        data_flux = hdu[1].data['FLUX']
+        data_flux_err = hdu[1].data['FLUX_ERR']
+        data_quality = hdu[1].data['QUALITY']
+        data_time = data_time[np.where(data_quality == 0)]
+        data_flux = data_flux[np.where(data_quality == 0), :, :][0]
+        data_flux_err = data_flux_err[np.where(data_quality == 0), :, :][0]
+        self.wcs = wcs
+        self.time = data_time
+        self.flux = data_flux
+        self.flux_err = data_flux_err
+        self.quality = np.zeros(len(data_time))
+
+        gaia_targets = self.catalogdata[
+            'designation', 'phot_g_mean_mag', 'phot_bp_mean_mag', 'phot_rp_mean_mag', 'ra', 'dec']
+        x = np.zeros(len(gaia_targets))
+        y = np.zeros(len(gaia_targets))
+        tess_mag = np.zeros(len(gaia_targets))
+        in_frame = [True] * len(gaia_targets)
+        for i, designation in enumerate(gaia_targets['designation']):
+            pixel = self.wcs.all_world2pix(
+                np.array([gaia_targets['ra'][i], gaia_targets['dec'][i]]).reshape((1, 2)), 0)
+            x[i] = pixel[0][0]
+            y[i] = pixel[0][1]
+            if -2 < x[i] < self.size + 1 and -2 < y[i] < self.size + 1:
+                dif = gaia_targets['phot_bp_mean_mag'][i] - gaia_targets['phot_rp_mean_mag'][i]
+                tess_mag[i] = gaia_targets['phot_g_mean_mag'][
+                                  i] - 0.00522555 * dif ** 3 + 0.0891337 * dif ** 2 - 0.633923 * dif + 0.0324473
+                if np.isnan(tess_mag[i]):
+                    tess_mag[i] = gaia_targets['phot_g_mean_mag'][i] - 0.430
+            else:
+                in_frame[i] = False
+        tess_flux = 10 ** (- tess_mag / 2.5)
+        t = Table()
+        t[f'tess_mag'] = tess_mag[in_frame]
+        t[f'tess_flux'] = tess_flux[in_frame]
+        t[f'tess_flux_ratio'] = tess_flux[in_frame] / np.max(tess_flux[in_frame])
+        t[f'sector_{self.sector}_x'] = x[in_frame]
+        t[f'sector_{self.sector}_y'] = y[in_frame]
+        gaia_targets = hstack([gaia_targets[in_frame], t])  # TODO: sorting not sorting all columns
+        gaia_targets.sort('tess_mag')
+        self.gaia = gaia_targets
+
+
+def ffi(target='', local_directory='', size=90, sector=None):
+    """
+    Function to generate Source_cut objects
+    :param target: string, required
+    target name
+    :param local_directory: string, required
+    output directory
+    :param size: int, required
+    FFI cut side length
+    :param sector: int, required
+    TESS sector number
+    :return: tglc.ffi_cut.Source_cut
+    """
+    source_exists = exists(f'{local_directory}source/source_{target}.pkl')
+    if source_exists:
+        with open(f'{local_directory}source/source_{target}.pkl', 'rb') as input_:
+            source = pickle.load(input_)
+        print(source.sector_table)
+        print('Loaded ffi from directory. ')
+    else:
+        with open(f'{local_directory}source/source_{target}.pkl', 'wb') as output:
+            source = Source_cut(target, size=size, sector=sector)
+            pickle.dump(source, output, pickle.HIGHEST_PROTOCOL)
+    return source
+
