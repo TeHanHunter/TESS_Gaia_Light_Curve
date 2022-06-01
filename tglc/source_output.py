12677cc6d3367371b68ec2c4a1e2e645d68d500f
Te Han <tehanhunter@gmail.com>
2022-05-31 23:56:03 -0700

    multiprocessing

--- tglc/source_output.py
+++ tglc/source_output.py
@@ -5,7 +5,7 @@ from multiprocessing import Pool
 from functools import partial
 
 
-def ffi_to_source(sector=1, ccd=1, local_directory=''):
+def ffi_to_source(sector=1, camera=1, local_directory=''):
     '''
     Cut calibrated FFI to source.pkl
     :param sector: int, required
@@ -20,10 +20,10 @@ def ffi_to_source(sector=1, ccd=1, local_directory=''):
     os.makedirs(local_directory + f'source/', exist_ok=True)
 
     with Pool() as p:
-      p.map(partial(cut_ffi, sector=sector, size=150, path=local_directory, ccd=ccd), [1, 2, 3, 4])
+      p.map(partial(cut_ffi, camera=camera, sector=sector, size=150, path=local_directory), [1, 2, 3, 4])
 
 
 if __name__ == '__main__':
     sector = 1
     for i in range(4):
-        ffi_to_source(sector=sector, ccd=i, local_directory=f'/home/tehan/data/sector{sector:04d}/')
+        ffi_to_source(sector=sector, camera=i, local_directory=f'/home/tehan/data/sector{sector:04d}/')
