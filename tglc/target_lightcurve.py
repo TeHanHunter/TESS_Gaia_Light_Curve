fbf614100b25070e4b94880ab9783a9cdeb0b100
Te Han <tehanhunter@gmail.com>
2022-06-03 21:51:41 -0700

    multiprocessing

--- tglc/target_lightcurve.py
+++ tglc/target_lightcurve.py
@@ -285,7 +285,7 @@ def epsf(source, psf_size=11, factor=2, local_directory='', target=None, cut_x=0
                       aperture=aperture.astype(np.float32), star_y=y_round[i], star_x=x_round[i], tglc_flag=quality,
                       bg=background_, time=source.time, psf_lc=psf_lc, cal_psf_lc=cal_psf_lc, aper_lc=aper_lc,
                       cal_aper_lc=cal_aper_lc, local_bg=local_bg, x_aperture=x_aperture[i], y_aperture=y_aperture[i],
-                      near_edge=near_edge, save_aper=False)
+                      near_edge=near_edge, save_aper=save_aper)
     # np.save(local_directory + f'mean_diff_aper_{target}.npy', np.array([mag, mean_diff_aper]))
     # np.save(local_directory + f'mean_diff_psf_{target}.npy', np.array([mag, mean_diff_psf]))
 
