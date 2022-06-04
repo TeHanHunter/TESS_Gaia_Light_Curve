a7394a03e4caccf84ffacede41ece9a5424eee2f
Te Han <tehanhunter@gmail.com>
2022-06-04 10:57:43 -0700

    Update run.py

--- tglc/run.py
+++ tglc/run.py
@@ -22,7 +22,7 @@ def lc_per_cut(i, ccd='1-1', local_directory=''):
     with open(local_directory + f'source/{ccd}/source_{cut_x:02d}_{cut_y:02d}.pkl', 'rb') as input_:
         source = pickle.load(input_)
     epsf(source, psf_size=11, factor=2, cut_x=cut_x, cut_y=cut_y, ccd=ccd, sector=source.sector,
-         local_directory=local_directory, limit_mag=16, save_aper=False)  # TODO: power?
+         local_directory=local_directory, limit_mag=16, save_aper=False, no_progress_bar=True)  # TODO: power?
 
 
 def lc_per_ccd(ccd='1-1', local_directory=''):
@@ -58,5 +58,6 @@ if __name__ == '__main__':
     names = ['1-1', '1-2', '1-3', '1-4']  # , '2-1', '2-2', '2-3', '2-4',
     # '3-1', '3-2', '3-3', '3-4', '4-1', '4-2', '4-3', '4-4']
     for name in names:
+        print(name)
         lc_per_ccd(ccd=name, local_directory=local_directory)
         plot_epsf(sector=sector, ccd=name, local_directory=local_directory)
