6dcece10d041df537f18b04e22891c5f6986f837
Te Han <tehanhunter@gmail.com>
2022-07-19 14:24:06 -0700

    variable star figure

--- tglc/quick_lc.py
+++ tglc/quick_lc.py
@@ -40,8 +40,9 @@ def tglc_lc(target='NGC 7654', local_directory='', size=90, save_aper=True, limi
 
 
 if __name__ == '__main__':
-    local_directory = '/mnt/c/users/tehan/desktop/pseudo/'
-    tglc_lc(target='TIC 269820902', local_directory=local_directory, size=50, save_aper=True, limit_mag=16,
+    target = input('Target ID or coordinates: ')
+    local_directory = '/home/tehan/data/cosmos/'
+    tglc_lc(target=target, local_directory=local_directory, size=90, save_aper=False, limit_mag=15,
             get_all_lc=True, first_sector_only=True)
 
     ####### list of targets example
