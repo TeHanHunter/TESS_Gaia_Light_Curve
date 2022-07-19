c8c62591ed77f32c3483e0d02fd1a56284390af3
Te Han <tehanhunter@gmail.com>
2022-07-19 12:01:29 -0700

    Update effective_psf.py

--- tglc/effective_psf.py
+++ tglc/effective_psf.py
@@ -256,13 +256,13 @@ def bg_mod(source, q=None, aper_lc=None, psf_lc=None, portion=None, star_num=0, 
     # centroid_to_aper_ratio = np.nanmedian(ratio)
     # flux_bar = aperture_bar * centroid_to_aper_ratio
     # lightcurve = lightcurve + (flux_bar - np.nanmedian(lightcurve[q]))
+    print(aper_lc)
     aperture_bar = bar * portion
     local_bg = np.nanmedian(aper_lc[q]) - aperture_bar
     aper_lc = aper_lc - local_bg
     psf_bar = bar
     local_bg = np.nanmedian(psf_lc[q]) - psf_bar
     psf_lc = psf_lc - local_bg
-    print(aper_lc)
     cal_aper_lc = flatten(source.time, aper_lc / np.nanmedian(aper_lc), window_length=1, method='biweight',
                           return_trend=False)
     if near_edge:
