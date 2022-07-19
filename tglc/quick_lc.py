2a83a873a0b3faa10cda1f95d0370219fed84b9e
Te Han <tehanhunter@gmail.com>
2022-07-19 11:52:22 -0700

    Update quick_lc.py

--- tglc/quick_lc.py
+++ tglc/quick_lc.py
@@ -36,7 +36,7 @@ def tglc_lc(target='NGC 7654', local_directory='', size=90, save_aper=True, limi
 
 if __name__ == '__main__':
     local_directory = '/home/tehan/data/cosmos/'
-    tglc_lc(target='TIC 269820902', local_directory=local_directory, size=50, save_aper=True, limit_mag=16, get_all_lc=False)
+    tglc_lc(target='TIC 269820902', local_directory=local_directory, size=50, save_aper=True, limit_mag=16, get_all_lc=True)
 
     ####### list of targets example
     # local_directory = '/home/tehan/data/ob_associations/'
