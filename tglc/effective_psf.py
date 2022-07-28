40fde26e5f2007714bee67d8fa46fba5bf3c09fe
Te Han <tehanhunter@gmail.com>
2022-07-28 13:09:01 -0700

    variable star figure

--- tglc/effective_psf.py
+++ tglc/effective_psf.py
@@ -226,11 +226,11 @@ def fit_lc(A, source, star_info=None, x=0., y=0., star_num=0, factor=2, psf_size
     A_[:, -3] = xx.flatten()
     # edge_pixel = np.array([0, 1, 2, 3, 4, 5, 9, 10, 14, 15, 19, 20, 21, 22, 23, 24])
     edge_pixel = np.array([0, 1, 2, 3, 4, 5, 6,
-                           7, 13,
-                           14, 20,
-                           21, 27,
-                           28, 34,
-                           35, 41,
+                           7, 8, 9, 10, 11, 12, 13,
+                           14, 15, 19, 20,
+                           21, 22, 26, 27,
+                           28, 29, 33, 34,
+                           35, 36, 37, 38, 39, 40, 41,
                            42, 43, 44, 45, 46, 47, 48])
     med_aperture = np.median(aperture, axis=0).flatten()
     outliers = np.abs(med_aperture[edge_pixel] - np.nanmedian(med_aperture[edge_pixel])) > 1 * np.std(
