bbe8b5633bbd0a356ea0379c1381a17b714771dc
Te Han <tehanhunter@gmail.com>
2022-09-15 19:49:41 -0700

    Update to Gaia DR3

--- tglc/target_lightcurve.py
+++ tglc/target_lightcurve.py
@@ -264,32 +264,33 @@ def epsf(source, psf_size=11, factor=2, local_directory='', target=None, cut_x=0
         end = start + 1
     for i in trange(start, end, desc='Fitting lc', disable=no_progress_bar):
         if x_left <= x_round[i] < source.size - x_right and y_left <= y_round[i] < source.size - y_right:
-            if 1.5 <= x_round[i] < source.size - 2.5 and 1.5 <= y_round[i] < source.size - 2.5:
-                near_edge = False
-            elif out_of_frame[x_round, y_round]:
+            if out_of_frame[y_round[i], x_round[i]]:
                 print(f"Skipping star {source.gaia['designation'][i]}, not in the frame")
                 continue
             else:
-                near_edge = True
-            aperture, psf_lc, star_y, star_x, portion = fit_lc(A, source, star_info=star_info, x=x_round[i],
-                                                               y=y_round[i], star_num=i, e_psf=e_psf,
-                                                               near_edge=near_edge)
-            aper_lc = np.sum(aperture[:, max(0, star_y - 1):min(5, star_y + 2), max(0, star_x - 1):min(5, star_x + 2)],
-                             axis=(1, 2))
-            local_bg, aper_lc, psf_lc, cal_aper_lc, cal_psf_lc = bg_mod(source, q=index, portion=portion, psf_lc=psf_lc,
-                                                                        aper_lc=aper_lc,
-                                                                        near_edge=near_edge, star_num=i)
-            # mag.append(source.gaia['tess_mag'][i])
-            # mean_diff_aper.append(np.nanmean(np.abs(np.diff(aper_lc[index])) / portion))
-            # mean_diff_psf.append(np.nanmean(np.abs(np.diff(psf_lc[index]))))
-            background_ = background[x_round[i] + source.size * y_round[i], :]
-            quality = np.zeros(len(source.time), dtype=np.int16)
-            sigma = 1.4826 * np.nanmedian(np.abs(background_ - np.nanmedian(background_)))
-            quality[abs(background_ - np.nanmedian(background_)) >= 5 * sigma] += 1
-            lc_output(source, local_directory=lc_directory, index=i,
-                      tess_flag=source.quality, cut_x=cut_x, cut_y=cut_y, cadence=source.cadence,
-                      aperture=aperture.astype(np.float32), star_y=y_round[i], star_x=x_round[i], tglc_flag=quality,
-                      bg=background_, time=source.time, psf_lc=psf_lc, cal_psf_lc=cal_psf_lc, aper_lc=aper_lc,
-                      cal_aper_lc=cal_aper_lc, local_bg=local_bg, x_aperture=x_aperture[i], y_aperture=y_aperture[i],
-                      near_edge=near_edge, save_aper=save_aper, portion=portion)
+                if 1.5 <= x_round[i] < source.size - 2.5 and 1.5 <= y_round[i] < source.size - 2.5:
+                    near_edge = False
+                else:
+                    near_edge = True
+                aperture, psf_lc, star_y, star_x, portion = fit_lc(A, source, star_info=star_info, x=x_round[i],
+                                                                   y=y_round[i], star_num=i, e_psf=e_psf,
+                                                                   near_edge=near_edge)
+                aper_lc = np.sum(aperture[:, max(0, star_y - 1):min(5, star_y + 2), max(0, star_x - 1):min(5, star_x + 2)],
+                                 axis=(1, 2))
+                local_bg, aper_lc, psf_lc, cal_aper_lc, cal_psf_lc = bg_mod(source, q=index, portion=portion, psf_lc=psf_lc,
+                                                                            aper_lc=aper_lc,
+                                                                            near_edge=near_edge, star_num=i)
+                # mag.append(source.gaia['tess_mag'][i])
+                # mean_diff_aper.append(np.nanmean(np.abs(np.diff(aper_lc[index])) / portion))
+                # mean_diff_psf.append(np.nanmean(np.abs(np.diff(psf_lc[index]))))
+                background_ = background[x_round[i] + source.size * y_round[i], :]
+                quality = np.zeros(len(source.time), dtype=np.int16)
+                sigma = 1.4826 * np.nanmedian(np.abs(background_ - np.nanmedian(background_)))
+                quality[abs(background_ - np.nanmedian(background_)) >= 5 * sigma] += 1
+                lc_output(source, local_directory=lc_directory, index=i,
+                          tess_flag=source.quality, cut_x=cut_x, cut_y=cut_y, cadence=source.cadence,
+                          aperture=aperture.astype(np.float32), star_y=y_round[i], star_x=x_round[i], tglc_flag=quality,
+                          bg=background_, time=source.time, psf_lc=psf_lc, cal_psf_lc=cal_psf_lc, aper_lc=aper_lc,
+                          cal_aper_lc=cal_aper_lc, local_bg=local_bg, x_aperture=x_aperture[i], y_aperture=y_aperture[i],
+                          near_edge=near_edge, save_aper=save_aper, portion=portion)
 
