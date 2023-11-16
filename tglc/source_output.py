f95fa434f5f60e8b12ff437c2fb3c32c0ecb2a13
Te Han <tehanhunter@gmail.com>
2023-11-16 14:24:41 -0500

    Update source_output.py

--- tglc/source_output.py
+++ tglc/source_output.py
@@ -66,7 +66,7 @@ def ffi_to_source(sector=1, local_directory=''):
     os.makedirs(f'{local_directory}log/', exist_ok=True)
     os.makedirs(f'{local_directory}mask/', exist_ok=True)
 
-    with Pool(4) as p:
+    with Pool(1) as p:
         p.map(partial(cut_ffi_, sector=sector, size=150, local_directory=local_directory), range(16))
 
     # for i in range(16):
