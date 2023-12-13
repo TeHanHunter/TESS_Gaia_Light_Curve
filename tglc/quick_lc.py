fa42694e49011522716412efe4f27f0e891a9e6d
tehanhunter <tehanhunter@gmail.com>
2023-12-13 15:03:42 -0800

    add mid_transit_tbjd in plot_pf_lc

--- tglc/quick_lc.py
+++ tglc/quick_lc.py
@@ -440,7 +440,11 @@ if __name__ == '__main__':
     directory = f'/home/tehan/data/cosmos/GEMS/'
     os.makedirs(directory, exist_ok=True)
     # get_tglc_lc(tics=tics, method='query', server=1, directory=directory)
-    # plot_lc(local_directory=f'{directory}TIC {tics[0]}/lc/', type='cal_aper_flux')
+    plot_lc(local_directory=f'{directory}TIC {tics[0]}/lc/', type='cal_psf_flux')
+    plot_lc(local_directory=f'{directory}TIC {tics[0]}/lc/', type='cal_aper_flux')
+
+    plot_pf_lc(local_directory=f'{directory}TIC {tics[0]}/lc/', period=0.71912603, mid_transit_tbjd=2790.58344,
+               type='cal_psf_flux')
     plot_pf_lc(local_directory=f'{directory}TIC {tics[0]}/lc/', period=0.71912603, mid_transit_tbjd=2790.58344,
                type='cal_aper_flux')
 
