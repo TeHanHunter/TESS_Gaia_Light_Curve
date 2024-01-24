114a7a6c07a7c4b19d8eeff4e2e80de388f1c217
Te Han <tehanhunter@gmail.com>
2024-01-24 11:09:28 -0500

    Start pdo test run on sector 23 cam1ccd1

--- tglc/run.py
+++ tglc/run.py
@@ -85,7 +85,7 @@ def plot_epsf(sector=1, camccd='', local_directory=''):
 
 if __name__ == '__main__':
     print("Number of cpu : ", multiprocessing.cpu_count())
-    sector = 1
+    sector = 23
     local_directory = f'/home/tehan/data/sector{sector:04d}/'
     # for i in range(16):
     #     name = f'{1 + i // 4}-{1 + i % 4}'
