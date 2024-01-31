374e253b1a10bceaf54de027a336ad0087e90fcc
Te Han <tehanhunter@gmail.com>
2024-01-30 16:18:17 -0800

    sort exoplanets

--- tglc/quick_lc.py
+++ tglc/quick_lc.py
@@ -517,7 +517,7 @@ if __name__ == '__main__':
     # directory = f'/home/tehan/Documents/GEMS/'
     directory = f'/home/tehan/data/cosmos/transit_depth_validation_extra/'
     os.makedirs(directory, exist_ok=True)
-    get_tglc_lc(tics=tics, method='search', server=2, directory=directory)
+    get_tglc_lc(tics=tics, method='search', server=1, directory=directory)
     # plot_lc(local_directory=f'{directory}TIC {tics[0]}/lc/', type='cal_psf_flux')
     # plot_lc(local_directory=f'{directory}TIC {tics[0]}/lc/', type='cal_aper_flux')
 
