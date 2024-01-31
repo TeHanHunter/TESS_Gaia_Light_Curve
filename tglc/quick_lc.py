50992cb34884176c60c09b3f737e18c563c4ad02
Te Han <tehanhunter@gmail.com>
2024-01-30 16:00:47 -0800

    sort exoplanets

--- tglc/quick_lc.py
+++ tglc/quick_lc.py
@@ -492,7 +492,7 @@ def get_tglc_lc(tics=None, method='query', server=1, directory=None, prior=None)
 
 if __name__ == '__main__':
     t = ascii.read(pkg_resources.resource_stream(__name__, 'PSCompPars_2024.01.30_15.54.52.csv'))
-    tics = [int(s[4:]) for s in t['tic_id']]
+    # tics = [int(s[4:]) for s in t['tic_id']]
     sort_sectors(t)
     # for i in range(len(t)):
     #
