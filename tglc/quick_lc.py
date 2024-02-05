93af9de9f1ea839a036771693dee912f21703a2e
tehanhunter <tehanhunter@gmail.com>
2024-02-05 14:44:40 -0800

    fitting NEA

--- tglc/quick_lc.py
+++ tglc/quick_lc.py
@@ -250,13 +250,12 @@ def produce_config(dir, tic=None, gaiadr3=None, nea=None, sector=1):
 def sort_sectors(t, dir='/home/tehan/data/cosmos/transit_depth_validation/'):
     tics = [int(s[4:]) for s in t['tic_id']]
     files = glob(f'{dir}*.fits')
-    tic_sector = np.empty(shape=(len(files), 3), dtype='str')
+    tic_sector = np.empty(shape=(len(files), 3), dtype='object')
     for i in range(len(files)):
         hdul = fits.open(files[i])
         tic_sector[i, 0] = str(hdul[0].header['TICID'])
         tic_sector[i, 1] = str(hdul[0].header['GAIADR3'])
         tic_sector[i, 2] = str(hdul[0].header['sector'])
-    print(str(hdul[0].header['TICID']))
     print('All stars produced:', set(tics) <= set(tic_sector[:, 0]))
     difference_set = set(tics) - set(tic_sector[:, 0])
     print("No. of stars in NEA but not in folder:", len(list(difference_set)))
