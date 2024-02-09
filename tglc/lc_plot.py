4da8edbb4a01744afb15e1c59c1dd37dd79291a7
Te Han <tehanhunter@gmail.com>
2024-02-08 17:25:18 -0800

    sort exoplanets

--- tglc/lc_plot.py
+++ tglc/lc_plot.py
@@ -63,11 +63,42 @@ def figure_1(folder='/home/tehan/data/pyexofits/Data/', ):
     plt.plot([0, 0.4], [0, 0.4], 'k')
     plt.errorbar(t_['pl_ratror'], t_['ror'],
                  yerr=[t_['ror'].data + t_['rorerr2'].data, t_['ror'].data + t_['rorerr1'].data * -1], fmt='o',
-                 mec='C2', mfc='none', ecolor='C2', ms=2, elinewidth=0.1, capsize=0.5)
+                 mec='C2', mfc='none', ecolor='C2', ms=4, elinewidth=0.1, capsize=2)
     plt.xlim(0, 0.1)
     plt.ylim(0, 0.1)
     plt.savefig(os.path.join(folder, 'ror_diagonal.png'), bbox_inches='tight', dpi=600)
 
+
+def figure_2(folder='/home/tehan/data/pyexofits/Data/', ):
+    t = ascii.read(pkg_resources.resource_stream(__name__, 'PSCompPars_2024.02.05_22.52.50.csv'))
+    tics = [int(s[4:]) for s in t['tic_id']]
+
+    t_ = Table(names=['pl_ratror', 'pl_ratrorerr1', 'pl_ratrorerr2', 'ror', 'rorerr1', 'rorerr2'],
+               dtype=['f8', 'f8', 'f8', 'f8', 'f8', 'f8'])
+    for i in trange(len(tics)):
+        file = glob(os.path.join(folder, f'*/Photometry/*/*{tics[i]}*.dat'))
+        if len(file) == 0:
+            pass
+        elif len(file) >= 1:
+            for j in range(len(file)):
+                star = int(os.path.basename(file[j]).split('_')[2])
+                if star == tics[i]:
+                    table = read_parameter(file[j])
+                    table_ror = table[table['Parameter'] == 'ror__0']
+                    # t_.add_row([t['pl_ratror'][i], t['pl_ratrorerr1'][i], t['pl_ratrorerr2'][i],
+                    #             table_ror['Value'][0], table_ror['Upper Error'][0], table_ror['Lower Error'][0]])
+                    t_.add_row(
+                        [t['pl_rade'][i] / t['st_rad'][i] / 109.076, t['pl_ratrorerr1'][i], t['pl_ratrorerr2'][i],
+                         table_ror['Value'][0], table_ror['Upper Error'][0], table_ror['Lower Error'][0]])
+    print(len(t_))
+    plt.figure(figsize=(10, 10))
+    plt.plot([0, 0.4], [0, 0.4], 'k')
+    plt.errorbar(t_['pl_ratror'], t_['ror'],
+                 yerr=[t_['ror'].data + t_['rorerr2'].data, t_['ror'].data + t_['rorerr1'].data * -1], fmt='o',
+                 mec='C2', mfc='none', ecolor='C2', ms=2, elinewidth=0.1, capsize=0.5)
+    plt.xlim(0, 0.1)
+    plt.ylim(0, 0.1)
+    plt.savefig(os.path.join(folder, 'ror_diagonal.png'), bbox_inches='tight', dpi=600)
 
 if __name__ == '__main__':
     figure_1()
