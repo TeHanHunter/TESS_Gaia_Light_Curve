13339093b7c9ebac488aa70254e6448915c9c3f3
Te Han <tehanhunter@gmail.com>
2024-02-08 17:29:54 -0800

    sort exoplanets

--- tglc/lc_plot.py
+++ tglc/lc_plot.py
@@ -62,8 +62,8 @@ def figure_1(folder='/home/tehan/data/pyexofits/Data/', ):
     plt.figure(figsize=(10, 10))
     plt.plot([0, 0.4], [0, 0.4], 'k')
     plt.errorbar(t_['pl_ratror'], t_['ror'],
-                 yerr=[t_['ror'].data + t_['rorerr2'].data, t_['ror'].data + t_['rorerr1'].data * -1], fmt='o',
-                 mec='C2', mfc='none', ecolor='C2', ms=4, elinewidth=0.1, capsize=2)
+                 yerr=[t_['ror'].data + t_['rorerr2'].data, t_['ror'].data + t_['rorerr1'].data], fmt='o',
+                 mec='C2', mfc='none', ecolor='C2', ms=2, elinewidth=0.1, capsize=0.5)
     plt.xlim(0, 0.1)
     plt.ylim(0, 0.1)
     plt.savefig(os.path.join(folder, 'ror_diagonal.png'), bbox_inches='tight', dpi=600)
@@ -73,7 +73,7 @@ def figure_2(folder='/home/tehan/data/pyexofits/Data/', ):
     t = ascii.read(pkg_resources.resource_stream(__name__, 'PSCompPars_2024.02.05_22.52.50.csv'))
     tics = [int(s[4:]) for s in t['tic_id']]
 
-    t_ = Table(names=['pl_ratror', 'pl_ratrorerr1', 'pl_ratrorerr2', 'ror', 'rorerr1', 'rorerr2'],
+    t_ = Table(names=['pl_rade', 'pl_radeerr1', 'pl_radeerr2', 'pl_r', 'pl_rerr1', 'pl_rerr2'],
                dtype=['f8', 'f8', 'f8', 'f8', 'f8', 'f8'])
     for i in trange(len(tics)):
         file = glob(os.path.join(folder, f'*/Photometry/*/*{tics[i]}*.dat'))
@@ -84,17 +84,17 @@ def figure_2(folder='/home/tehan/data/pyexofits/Data/', ):
                 star = int(os.path.basename(file[j]).split('_')[2])
                 if star == tics[i]:
                     table = read_parameter(file[j])
-                    table_ror = table[table['Parameter'] == 'ror__0']
+                    table_ror = table[table['Parameter'] == 'r_pl__0']
                     # t_.add_row([t['pl_ratror'][i], t['pl_ratrorerr1'][i], t['pl_ratrorerr2'][i],
                     #             table_ror['Value'][0], table_ror['Upper Error'][0], table_ror['Lower Error'][0]])
                     t_.add_row(
-                        [t['pl_rade'][i] / t['st_rad'][i] / 109.076, t['pl_ratrorerr1'][i], t['pl_ratrorerr2'][i],
+                        [t['pl_rade'][i], t['pl_radeerr1'][i], t['pl_radeerr2'][i],
                          table_ror['Value'][0], table_ror['Upper Error'][0], table_ror['Lower Error'][0]])
     print(len(t_))
     plt.figure(figsize=(10, 10))
     plt.plot([0, 0.4], [0, 0.4], 'k')
-    plt.errorbar(t_['pl_ratror'], t_['ror'],
-                 yerr=[t_['ror'].data + t_['rorerr2'].data, t_['ror'].data + t_['rorerr1'].data * -1], fmt='o',
+    plt.errorbar(t_['pl_rade'], t_['pl_r'],
+                 yerr=[t_['pl_r'].data + t_['pl_rerr2'].data, t_['pl_r'].data + t_['pl_rerr1'].data], fmt='o',
                  mec='C2', mfc='none', ecolor='C2', ms=2, elinewidth=0.1, capsize=0.5)
     plt.xlim(0, 0.1)
     plt.ylim(0, 0.1)
