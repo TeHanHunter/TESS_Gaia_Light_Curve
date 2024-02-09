94eb54a97dad2cbb416eccd31d67c72a08f522d3
Te Han <tehanhunter@gmail.com>
2024-02-08 17:47:04 -0800

    sort exoplanets

--- tglc/lc_plot.py
+++ tglc/lc_plot.py
@@ -18,6 +18,7 @@ import pkg_resources
 import seaborn as sns
 import matplotlib.cm as cm
 
+
 def read_parameter(file=None):
     with open(file, 'r') as file:
         lines = file.readlines()
@@ -36,44 +37,12 @@ def read_parameter(file=None):
                            float(lines[i].split(',')[1].split('$')[2])])
     return table
 
-
-def figure_1(folder='/home/tehan/data/pyexofits/Data/', ):
+def figure_1(folder='/home/tehan/data/pyexofits/Data/', param='pl_rade'):
+    param_dict = {'pl_rade': 'r_pl__0', 'pl_ratror': 'ror__0'}
     t = ascii.read(pkg_resources.resource_stream(__name__, 'PSCompPars_2024.02.05_22.52.50.csv'))
     tics = [int(s[4:]) for s in t['tic_id']]
 
-    t_ = Table(names=['pl_ratror', 'pl_ratrorerr1', 'pl_ratrorerr2', 'ror', 'rorerr1', 'rorerr2'],
-               dtype=['f8', 'f8', 'f8', 'f8', 'f8', 'f8'])
-    for i in trange(len(tics)):
-        file = glob(os.path.join(folder, f'*/Photometry/*/*{tics[i]}*.dat'))
-        if len(file) == 0:
-            pass
-        elif len(file) >= 1:
-            for j in range(len(file)):
-                star = int(os.path.basename(file[j]).split('_')[2])
-                if star == tics[i]:
-                    table = read_parameter(file[j])
-                    table_ror = table[table['Parameter'] == 'ror__0']
-                    # t_.add_row([t['pl_ratror'][i], t['pl_ratrorerr1'][i], t['pl_ratrorerr2'][i],
-                    #             table_ror['Value'][0], table_ror['Upper Error'][0], table_ror['Lower Error'][0]])
-                    t_.add_row(
-                        [t['pl_rade'][i] / t['st_rad'][i] / 109.076, t['pl_ratrorerr1'][i], t['pl_ratrorerr2'][i],
-                         table_ror['Value'][0], table_ror['Upper Error'][0], table_ror['Lower Error'][0]])
-    print(len(t_))
-    plt.figure(figsize=(10, 10))
-    plt.plot([0, 0.4], [0, 0.4], 'k')
-    plt.errorbar(t_['pl_ratror'], t_['ror'],
-                 yerr=[t_['rorerr2'].data * -1, t_['rorerr1'].data], fmt='o',
-                 mec='C2', mfc='none', ecolor='C2', ms=2, elinewidth=0.1, capsize=0.5)
-    plt.xlim(0, 0.1)
-    plt.ylim(0, 0.1)
-    plt.savefig(os.path.join(folder, 'ror_diagonal.png'), bbox_inches='tight', dpi=600)
-
-
-def figure_2(folder='/home/tehan/data/pyexofits/Data/', ):
-    t = ascii.read(pkg_resources.resource_stream(__name__, 'PSCompPars_2024.02.05_22.52.50.csv'))
-    tics = [int(s[4:]) for s in t['tic_id']]
-
-    t_ = Table(names=['Tmag', 'pl_rade', 'pl_radeerr1', 'pl_radeerr2', 'pl_r', 'pl_rerr1', 'pl_rerr2'],
+    t_ = Table(names=['Tmag', f'{param}', f'{param}err1', f'{param}err2', 'value', 'err1', 'err2'],
                dtype=['f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8'])
     for i in trange(len(tics)):
         file = glob(os.path.join(folder, f'*/Photometry/*/*{tics[i]}*.dat'))
@@ -84,26 +53,26 @@ def figure_2(folder='/home/tehan/data/pyexofits/Data/', ):
                 star = int(os.path.basename(file[j]).split('_')[2])
                 if star == tics[i]:
                     table = read_parameter(file[j])
-                    table_ror = table[table['Parameter'] == 'r_pl__0']
+                    table_ror = table[table['Parameter'] == param_dict['pl_rade']]
                     # t_.add_row([t['pl_ratror'][i], t['pl_ratrorerr1'][i], t['pl_ratrorerr2'][i],
                     #             table_ror['Value'][0], table_ror['Upper Error'][0], table_ror['Lower Error'][0]])
                     t_.add_row(
-                        [t['sy_tmag'][i], t['pl_rade'][i], t['pl_radeerr1'][i], t['pl_radeerr2'][i],
+                        [t['sy_tmag'][i], t[f'{param}'][i], t[f'{param}err1'][i], t[f'{param}err2'][i],
                          table_ror['Value'][0], table_ror['Upper Error'][0], table_ror['Lower Error'][0]])
     print(len(t_))
     plt.figure(figsize=(10, 10))
     colormap = cm.viridis
     norm = plt.Normalize(t_['Tmag'].min(), t_['Tmag'].max())
-    errorbar = plt.errorbar(t_['pl_rade'], t_['pl_r'],
-                            yerr=[t_['pl_rerr2'].data * -1, t_['pl_rerr1'].data],
-                            fmt='o', mec=colormap(norm(t_['pl_r'])), mfc='none',
-                            ecolor=colormap(norm(t_['pl_r'])), ms=2, elinewidth=0.1, capsize=0.5)
-    plt.colorbar(errorbar , label='TESS magnitude')
+    errorbar = plt.errorbar(t_[f'{param}'], t_['value'],
+                            yerr=[t_['err2'].data * -1, t_['err1'].data],
+                            fmt='o', mec=colormap(norm(t_['Tmag'])), mfc='none',
+                            ecolor=colormap(norm(t_['Tmag'])), ms=2, elinewidth=0.1, capsize=0.5)
+    plt.colorbar(errorbar, label='TESS magnitude')
     plt.plot([0, 40], [0, 40], 'k')
     plt.xlim(0, 25)
     plt.ylim(0, 25)
-    plt.savefig(os.path.join(folder, 'pl_rade_diagonal.png'), bbox_inches='tight', dpi=600)
+    plt.savefig(os.path.join(folder, f'{param}_diagonal.png'), bbox_inches='tight', dpi=600)
 
 
 if __name__ == '__main__':
-    figure_2()
+    figure_1()
