6b871bf5502660999ac098dbb888dbc9b36e40f2
tehanhunter <tehanhunter@gmail.com>
2024-02-15 00:45:08 -0800

    fitting NEA for qlp

--- tglc/lc_plot.py
+++ tglc/lc_plot.py
@@ -41,6 +41,7 @@ def read_parameter(file=None):
 def figure_1(folder='/home/tehan/Downloads/Data/', param='pl_rade', r1=0.01, r2=0.4, cmap='Tmag'):
     param_dict = {'pl_rade': 'r_pl__0', 'pl_ratror': 'ror__0'}
     t = ascii.read(pkg_resources.resource_stream(__name__, 'PSCompPars_2024.02.05_22.52.50.csv'))
+    # t = ascii.read('/home/tehan/PycharmProjects/TESS_Gaia_Light_Curve/tglc/PSCompPars_2024.02.05_22.52.50.csv')
     tics = [int(s[4:]) for s in t['tic_id']]
 
     t_ = Table(names=['Tmag', 'rhat', 'p', f'{param}', f'{param}err1', f'{param}err2', 'value', 'err1', 'err2'],
@@ -108,12 +109,12 @@ def figure_1(folder='/home/tehan/Downloads/Data/', param='pl_rade', r1=0.01, r2=
     axins.set_yticklabels(['0.07','','', '0.1', '0.12'])
     axins.plot([0.01, 40], [0.01, 40], 'k', zorder=0)
     mark_inset(ax, axins, loc1=1, loc2=3, fc="none", ec="0.5", linestyle='dashed')
-    plt.colorbar(scatter, ax=ax, label=cmap)
+    plt.colorbar(scatter, ax=ax, label='TESS magnitude')
     ax.plot([0.01, 40], [0.01, 40], 'k', zorder=0)
     ax.set_xlim(r1, r2)
     ax.set_ylim(r1, r2)
-    ax.set_xlabel(param)
-    ax.set_ylabel(param_dict[f'{param}'])
+    ax.set_xlabel(r'Literature $R_p/R_*$')
+    ax.set_ylabel(r'TGLC-only fit $R_p/R_*$')
     ax.set_xscale('log')
     ax.set_yscale('log')
     plt.savefig(os.path.join(folder, f'{param}_diagonal_.png'), bbox_inches='tight', dpi=600)
@@ -183,4 +184,4 @@ def figure_2(folder='/home/tehan/Downloads/Data/', param='pl_rade', r=25, cmap='
 
 
 if __name__ == '__main__':
-    figure_1(param='pl_ratror', cmap='Tmag')
+    figure_1(folder='/home/tehan/Downloads/Data/', param='pl_ratror', cmap='Tmag')
