6b045a088fe65408bf76f4d4644921df06cc952f
Te Han <tehanhunter@gmail.com>
2024-02-15 16:58:02 -0800

    sort exoplanets

--- tglc/lc_plot.py
+++ tglc/lc_plot.py
@@ -117,7 +117,7 @@ def figure_1(folder='/home/tehan/Downloads/Data/', param='pl_rade', r1=0.01, r2=
     ax.set_ylabel(r'TGLC-only fit $R_p/R_*$')
     ax.set_xscale('log')
     ax.set_yscale('log')
-    plt.savefig(os.path.join(folder, f'{param}_diagonal_qlp.png'), bbox_inches='tight', dpi=600)
+    plt.savefig(os.path.join(folder, f'{param}_diagonal.png'), bbox_inches='tight', dpi=600)
 
 
 def figure_2(folder='/home/tehan/Downloads/Data/', param='pl_rade', r=25, cmap='Tmag'):
@@ -180,6 +180,87 @@ def figure_2(folder='/home/tehan/Downloads/Data/', param='pl_rade', r=25, cmap='
     plt.ylabel(param_dict[f'{param}'])
     plt.xscale('log')
     plt.yscale('log')
+    plt.savefig(os.path.join(folder, f'{param}_diagonal.png'), bbox_inches='tight', dpi=600)
+
+def figure_3(folder='/home/tehan/Downloads/Data/', param='pl_rade', r1=0.0001, r2=0.16, cmap='Tmag'):
+    param_dict = {'pl_rade': 'r_pl__0', 'pl_ratror': 'ror__0'}
+    t = ascii.read(pkg_resources.resource_stream(__name__, 'PSCompPars_2024.02.05_22.52.50.csv'))
+    # t = ascii.read('/home/tehan/PycharmProjects/TESS_Gaia_Light_Curve/tglc/PSCompPars_2024.02.05_22.52.50.csv')
+    tics = [int(s[4:]) for s in t['tic_id']]
+
+    t_ = Table(names=['Tmag', 'rhat', 'p', f'{param}', f'{param}err1', f'{param}err2', 'value', 'err1', 'err2'],
+               dtype=['f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8'])
+    missed_stars = 0
+    for i in trange(len(tics)):
+        file = glob(os.path.join(folder, f'*/Photometry/*/*{tics[i]}*.dat'))
+        if len(file) == 0:
+            missed_stars += 1
+        elif len(file) >= 1:
+            for j in range(len(file)):
+                star = int(os.path.basename(file[j]).split('_')[2])
+                if star == tics[i]:
+                    table_posterior = read_parameter(file[j])
+                    table_posterior_row = table_posterior[table_posterior['Parameter'] == param_dict[param]]
+                    chain_summary = glob(os.path.join(os.path.dirname(file[j]), 'ChainSummary*.csv'))
+                    table_chain = Table.read(chain_summary[0], format='csv')
+                    table_chain_row = table_chain[table_chain['Parameter'] == param_dict[param][0:-3] + '[0]']
+
+                    if param == 'pl_rade':
+                        t_.add_row([t['sy_tmag'][i], table_chain_row['r_hat'], t['pl_orbper'][i], t[f'{param}'][i],
+                                    t[f'{param}err1'][i], t[f'{param}err2'][i], table_posterior_row['Value'][0],
+                                    table_posterior_row['Upper Error'][0], table_posterior_row['Lower Error'][0]])
+                    elif param == 'pl_ratror':
+                        ror = t['pl_rade'][i] / t['st_rad'][i] / 109.076
+                        sigma_rade = (t['pl_radeerr1'][i] - t['pl_radeerr2'][i]) / 2
+                        sigma_st_rad = (t['st_raderr1'][i] - t['st_raderr2'][i]) / 2
+                        sigma_ror = ((sigma_rade / t['st_rad'][i] / 109.076) ** 2 +
+                                     (t['pl_rade'][i] / t['st_rad'][i] ** 2 / 109.076 * sigma_st_rad) ** 2) ** 0.5
+                        t_.add_row(
+                            [t['sy_tmag'][i], table_chain_row['r_hat'], t['pl_orbper'][i], ror,
+                             sigma_ror, - sigma_ror, table_posterior_row['Value'][0],
+                             table_posterior_row['Upper Error'][0], table_posterior_row['Lower Error'][0]])
+    print(len(t_))
+    print('missing stars:', missed_stars)
+    colormap = cm.viridis
+    norm = plt.Normalize(t_[cmap].min(), t_[cmap].max())
+    scatter = plt.scatter(t_[f'{param}'], t_['value'], c=t_[cmap], cmap=colormap, facecolors='none', s=0)
+    fig, ax = plt.subplots(figsize=(12, 8))
+    for k in range(len(t_)):
+        if t_['rhat'][k] < 1.05:
+            ax.errorbar(t_[f'{param}'][k] ** 2, t_['value'][k] ** 2, xerr=2 * t_[f'{param}'][k] * t_[f'{param}err1'][k],
+                        yerr=(t_['err1'][k] - t_['err2'][k]) * t_['value'][k], fmt='o', mec=colormap(norm(t_[cmap][k])),
+                        mfc='none', ecolor=colormap(norm(t_[cmap][k])), ms=10, elinewidth=1, capsize=0.7, alpha=0.5,
+                        zorder=2)
+        # else:
+        #     plt.errorbar(t_[f'{param}'][k], t_['value'][k], yerr=[[t_['err2'][k] * -1], [t_['err1'][k]]],
+        #                  fmt='o', mec='silver', mfc='none', ecolor='silver',
+        #                  ms=10, elinewidth=1, capsize=5, alpha=0.8, zorder=1)
+    range_zoom = [0.07 ** 2, 0.12 ** 2]
+    axins = inset_axes(ax, width='35%', height='35%', loc='upper left', borderpad=2)
+    for k in range(len(t_)):
+        if t_['rhat'][k] < 1.05:
+            ax.errorbar(t_[f'{param}'][k] ** 2, t_['value'][k] ** 2, xerr=2 * t_[f'{param}'][k] * t_[f'{param}err1'][k],
+                        yerr=(t_['err1'][k] - t_['err2'][k]) * t_['value'][k], fmt='o', mec=colormap(norm(t_[cmap][k])),
+                        mfc='none', ecolor=colormap(norm(t_[cmap][k])), ms=10, elinewidth=1, capsize=0.7, alpha=0.5,
+                        zorder=2)
+    axins.set_xlim(range_zoom)
+    axins.set_ylim(range_zoom)
+    axins.set_xscale('log')
+    axins.set_yscale('log')
+    # axins.set_xticks([0.07,0.08,0.09, 0.1, 0.12])
+    # axins.set_xticklabels(['0.07','','', '0.1', '0.12'])
+    # axins.set_yticks([0.07,0.08,0.09, 0.1, 0.12])
+    # axins.set_yticklabels(['0.07','','', '0.1', '0.12'])
+    axins.plot([0.0001, 40], [0.0001, 40], 'k', zorder=0)
+    mark_inset(ax, axins, loc1=1, loc2=3, fc="none", ec="0.5", linestyle='dashed')
+    plt.colorbar(scatter, ax=ax, label='TESS magnitude')
+    ax.plot([0.0001, 40], [0.0001, 40], 'k', zorder=0)
+    ax.set_xlim(r1, r2)
+    ax.set_ylim(r1, r2)
+    ax.set_xlabel(r'Literature transit depth')
+    ax.set_ylabel(r'TGLC-only fit transit depth')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
     plt.savefig(os.path.join(folder, f'{param}_diagonal.png'), bbox_inches='tight', dpi=600)
 
 
