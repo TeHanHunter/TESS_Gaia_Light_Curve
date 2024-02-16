1ef894cbe61a836c72cc7f9a889ce655e222bc5d
Te Han <tehanhunter@gmail.com>
2024-02-15 16:50:11 -0800

    sort exoplanets

--- tglc/lc_plot.py
+++ tglc/lc_plot.py
@@ -92,7 +92,7 @@ def figure_1(folder='/home/tehan/Downloads/Data/', param='pl_rade', r1=0.01, r2=
         #                  fmt='o', mec='silver', mfc='none', ecolor='silver',
         #                  ms=10, elinewidth=1, capsize=5, alpha=0.8, zorder=1)
     range_zoom = [0.07, 0.12]
-    axins = inset_axes(ax, width='35%', height='35%', loc='lower right', borderpad=2)
+    axins = inset_axes(ax, width='35%', height='35%', loc='upper left', borderpad=2)
     for k in range(len(t_)):
         if t_['rhat'][k] < 1.05:
             axins.errorbar(t_[f'{param}'][k], t_['value'][k], xerr=t_[f'{param}err1'][k],
