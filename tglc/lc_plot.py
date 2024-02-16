0a6213eb42f37ba09c134e7b8b28ecdaed13253b
tehanhunter <tehanhunter@gmail.com>
2024-02-16 00:21:29 -0800

    fitting NEA for qlp

--- tglc/lc_plot.py
+++ tglc/lc_plot.py
@@ -130,8 +130,8 @@ def figure_1(folder='/home/tehan/Downloads/Data/', param='pl_rade', r1=0.01, r2=
 
     plt.figure(figsize=(5, 5))
     percent_err = (t_['err1'] - t_['err2']) / 2 / t_['value']
-    sort = np.argsort(np.array(t_['Tmag']))
     plt.scatter(t_['Tmag'], percent_err, c='k', s=1)
+    sort = np.argsort(np.array(t_['Tmag']))
     plt.plot(np.array(t_['Tmag'][sort])[2:-1], np.convolve(percent_err[sort], np.ones(5)/5, mode='valid'))
     plt.ylim(0,1)
     plt.xlabel('Tmag')
