87060330d5d14fbc921017af89240cd6c7038c8f
tehanhunter <tehanhunter@gmail.com>
2024-02-25 14:21:08 -0800

    michelle

--- tglc/quick_lc.py
+++ tglc/quick_lc.py
@@ -457,8 +457,8 @@ if __name__ == '__main__':
     directory = f'/home/tehan/data/cosmos/michelle/'
     os.makedirs(directory, exist_ok=True)
     get_tglc_lc(tics=tics, method='query', server=1, directory=directory)
-    plot_lc(local_directory=f'{directory}TIC {tics[0]}/', type='cal_aper_flux')
-    plot_contamination(local_directory=f'{directory}TIC {tics[0]}/', gaia_dr3=4041835225186613120)
+    # plot_lc(local_directory=f'{directory}TIC {tics[0]}/', type='cal_aper_flux')
+    plot_contamination(local_directory=f'{directory}TIC {tics[0]}/', gaia_dr3=4041831235071242624)
     # plot_epsf(local_directory=f'{directory}TIC {tics[0]}/')
     # plot_pf_lc(local_directory=f'{directory}TIC {tics[0]}/lc/', period=0.71912603, mid_transit_tbjd=2790.58344,
     #            type='cal_psf_flux')
