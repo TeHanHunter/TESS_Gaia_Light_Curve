674b91c8989e2637ea41ad0292db9f8d124665cd
Te Han <tehanhunter@gmail.com>
2024-03-05 12:58:37 -0800

    sort exoplanets

--- tglc/lc_plot.py
+++ tglc/lc_plot.py
@@ -2688,21 +2688,23 @@ def get_MAD_qlp(i, files=None):
             qlp_f = np.mean(lc[:len(lc) // 9 * 9].reshape(-1, 9), axis=1)
             MAD_qlp = np.median(np.abs(np.diff(qlp_f)))
             qlp_precision = 1.48 * MAD_qlp / (np.sqrt(2) * 1.5e4 * 10 ** ((10 - tic) / 2.5))
+            return tic, qlp_precision
+
         except:
             pass
     # np.save('/pdo/users/tehan/sector0056/mad_tglc_30min.npy', np.vstack((tic, aper_precision)))
-    return tic, qlp_precision
 
 def plot_MAD():
-    mad = np.load('/home/tehan/Downloads/mad_180.npy')
+    # mad = np.load('/home/tehan/Downloads/mad_180.npy')
+    mad = np.load('/mnt/c/users/tehan/Desktop/mad_tglc_30min.npy')
     sorted_indices = np.argsort(mad[0])
     mad = mad[:, sorted_indices]
-    noise_2015 = ascii.read('/home/tehan/Documents/tglc/prior_mad/noisemodel.dat')
+    # noise_2015 = ascii.read('/home/tehan/Documents/tglc/prior_mad/noisemodel.dat')
     fig, ax = plt.subplots(2, 1, sharex=True, gridspec_kw=dict(height_ratios=[3, 2], hspace=0.1, wspace=0.05),
                            figsize=(5, 5))
-    ax[0].plot(mad[0], mad[3], '.', c='tomato', ms=0.1, label='TGLC Weighted', alpha=0.01)
+    ax[0].plot(mad[0], mad[1], '.', c='tomato', ms=0.1, label='TGLC Weighted', alpha=0.01)
 
-    ax[0].plot(noise_2015['col1'], noise_2015['col2'], c='k', ms=1.5, label='Sullivan (2015)', alpha=1)
+    # ax[0].plot(noise_2015['col1'], noise_2015['col2'], c='k', ms=1.5, label='Sullivan (2015)', alpha=1)
     # # ax[0].plot(mean_diff_aper[0], aper_precision, 'D', c='r', ms=1, label='TGLC Aper', alpha=0.8)
     ax[0].hlines(y=.1, xmin=8, xmax=np.max(mad[0]), colors='k', linestyles='dotted')
     ax[0].hlines(y=.01, xmin=8, xmax=np.max(mad[0]), colors='k', linestyles='dotted')
@@ -2715,42 +2717,43 @@ def plot_MAD():
     ax[0].set_ylim(1e-4, 1)
     ax[0].set_title('TESSminer 180-min bin')
 
-    psf_ratio = mad[2] / mad[3]
-    psf_tglc_mag = mad[0][np.invert(np.isnan(psf_ratio))]
-    psf_ratio = psf_ratio[np.invert(np.isnan(psf_ratio))]
-    psf_runningmed = ndimage.median_filter(psf_ratio, size=2000, mode='nearest')
-
-    aper_ratio = mad[1] / mad[3]
-    aper_tglc_mag = mad[0][np.invert(np.isnan(aper_ratio))]
-    aper_ratio = aper_ratio[np.invert(np.isnan(aper_ratio))]
-    aper_runningmed = ndimage.median_filter(aper_ratio, size=2000, mode='nearest')
-
-    ax[1].plot(psf_tglc_mag[:-1000], psf_ratio[:-1000], '.', c='C1', ms=6, alpha=0.01,
-               label='TGLC PSF Precision/TGLC Weighted Precision')
-    ax[1].plot(aper_tglc_mag[:-1000], aper_ratio[:-1000], '.', c='C0', ms=6, alpha=0.01,
-               label='TGLC Aperture Precision/TGLC Weighted Precision')
-    ax[1].plot(psf_tglc_mag[:-1000], psf_runningmed[:-1000], c='C1', label='Median', lw=1.5,
-               path_effects=[pe.Stroke(linewidth=3, foreground='k'), pe.Normal()])
-    ax[1].plot(aper_tglc_mag[:-1000], aper_runningmed[:-1000], c='C0', label='Median', lw=1.5,
-               path_effects=[pe.Stroke(linewidth=3, foreground='k'), pe.Normal()])
-
-    ax[1].hlines(y=1, xmin=8, xmax=np.max(mad[0]), colors='k', linestyles='dotted')
-    # ax[1].set_yscale('log')
-    ax[1].set_ylim(0.5, 1.5)
-    ax[1].tick_params(axis='y', which='minor', labelleft=False)
-    ax[1].set_yticks(ticks=[0.5, 1, 1.5], labels=['0.5', '1', '1.5'])
-    # ax[1].set_title('Photometric Precision Ratio')
-    ax[1].set_xlabel('TESS magnitude')
-    ax[1].set_ylabel('Precision Ratio')
-    leg = ax[1].legend(loc=4, markerscale=1, ncol=2, columnspacing=1, fontsize=7.2)
-    for lh in leg.legendHandles:
-        lh.set_alpha(1)
+    # psf_ratio = mad[2] / mad[3]
+    # psf_tglc_mag = mad[0][np.invert(np.isnan(psf_ratio))]
+    # psf_ratio = psf_ratio[np.invert(np.isnan(psf_ratio))]
+    # psf_runningmed = ndimage.median_filter(psf_ratio, size=2000, mode='nearest')
+    #
+    # aper_ratio = mad[1] / mad[3]
+    # aper_tglc_mag = mad[0][np.invert(np.isnan(aper_ratio))]
+    # aper_ratio = aper_ratio[np.invert(np.isnan(aper_ratio))]
+    # aper_runningmed = ndimage.median_filter(aper_ratio, size=2000, mode='nearest')
+    #
+    # ax[1].plot(psf_tglc_mag[:-1000], psf_ratio[:-1000], '.', c='C1', ms=6, alpha=0.01,
+    #            label='TGLC PSF Precision/TGLC Weighted Precision')
+    # ax[1].plot(aper_tglc_mag[:-1000], aper_ratio[:-1000], '.', c='C0', ms=6, alpha=0.01,
+    #            label='TGLC Aperture Precision/TGLC Weighted Precision')
+    # ax[1].plot(psf_tglc_mag[:-1000], psf_runningmed[:-1000], c='C1', label='Median', lw=1.5,
+    #            path_effects=[pe.Stroke(linewidth=3, foreground='k'), pe.Normal()])
+    # ax[1].plot(aper_tglc_mag[:-1000], aper_runningmed[:-1000], c='C0', label='Median', lw=1.5,
+    #            path_effects=[pe.Stroke(linewidth=3, foreground='k'), pe.Normal()])
+    #
+    # ax[1].hlines(y=1, xmin=8, xmax=np.max(mad[0]), colors='k', linestyles='dotted')
+    # # ax[1].set_yscale('log')
+    # ax[1].set_ylim(0.5, 1.5)
+    # ax[1].tick_params(axis='y', which='minor', labelleft=False)
+    # ax[1].set_yticks(ticks=[0.5, 1, 1.5], labels=['0.5', '1', '1.5'])
+    # # ax[1].set_title('Photometric Precision Ratio')
+    # ax[1].set_xlabel('TESS magnitude')
+    # ax[1].set_ylabel('Precision Ratio')
+    # leg = ax[1].legend(loc=4, markerscale=1, ncol=2, columnspacing=1, fontsize=7.2)
+    # for lh in leg.legendHandles:
+    #     lh.set_alpha(1)
     plt.xlim(7, 20.5)
-    plt.savefig(f'/home/tehan/Documents/GEMS/tessminer_mad_180.png', bbox_inches='tight', dpi=300)
+    plt.savefig(f'/mnt/c/users/tehan/Desktop/s56_mad_30min.png', bbox_inches='tight', dpi=300)
     # point-to-point scatter
 
 
 if __name__ == '__main__':
+    # plot_MAD()
     files = glob('/pdo/users/tehan/qlp_s56/*.fits')
     print(len(files))
     with Pool() as p:
