b86e7aaf6e18fcaf0009fa3f5d25873e7c99c18d
tehan <tehanhunter@gmail.com>
2024-05-09 16:25:41 -0700

    lc_requests

--- tglc/quick_lc.py
+++ tglc/quick_lc.py
@@ -638,7 +638,8 @@ def get_tglc_lc(tics=None, method='query', server=1, directory=None, prior=None,
 
 if __name__ == '__main__':
     tics = [259172249]
-    directory = f'/Users/tehan/Documents/TGLC/'
+    # directory = f'/Users/tehan/Documents/TGLC/'
+    directory = '/home/tehan/data/cosmos/GEMS/'
     os.makedirs(directory, exist_ok=True)
     get_tglc_lc(tics=tics, directory=directory, ffi='SPOC')
     # plot_lc(local_directory=f'{directory}TIC {tics[0]}/', kind='cal_aper_flux')
