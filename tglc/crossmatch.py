dfa7fb3bf22997d927add6225c230d90df314871
tehan <tehanhunter@gmail.com>
2024-05-13 00:07:52 -0700

    mismatch

--- tglc/crossmatch.py
+++ tglc/crossmatch.py
@@ -5,7 +5,7 @@ from astropy.io import ascii
 
 def crossmatch(target_list='ListofMdwarfTICs_ForTe.csv', folder='/home/tehan/data/cosmos/GEMS/tessminer/'):
     files = glob.glob(folder + '*.fits')
-    targets = ascii.read(target_list)
+    targets = ascii.read(folder + target_list)
     gaia_dr3 = [target.split()[-1] for target in targets['designation']]
     occurances = np.zeros(len(gaia_dr3))
     for i in tqdm(gaia_dr3):
@@ -13,6 +13,7 @@ def crossmatch(target_list='ListofMdwarfTICs_ForTe.csv', folder='/home/tehan/dat
             if i in j:
                 occurances[i] += 1
     targets['occurances'] = occurances
+    ascii.write(targets, folder + 'ListofMdwarfTICs_crossmatch.csv', overwrite=True)
     return targets
 
 
