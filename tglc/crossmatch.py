bedf664309cf036bf70c2272a574214afbeb3945
tehan <tehanhunter@gmail.com>
2024-05-30 23:23:37 -0700

    mismatch

--- tglc/crossmatch.py
+++ tglc/crossmatch.py
@@ -3,6 +3,8 @@ from tqdm import tqdm
 import numpy as np
 from astropy.io import ascii
 from tqdm import trange
+
+
 def crossmatch(target_list='ListofMdwarfTICs_ForTe.csv', folder='/home/tehan/data/cosmos/GEMS/tessminer/'):
     files = glob.glob(folder + '*.fits')
     files = [file.split('-')[1] for file in files]
@@ -18,6 +20,7 @@ def crossmatch(target_list='ListofMdwarfTICs_ForTe.csv', folder='/home/tehan/dat
     targets['occurances'] = occurances
     ascii.write(targets, folder + 'ListofMdwarfTICs_crossmatch_even.csv', overwrite=True)
     return targets
+
 
 def combine(folder='/Users/tehan/Downloads/'):
     all = ascii.read(folder + 'ListofMdwarfTICs_ForTe.csv')
@@ -38,13 +41,19 @@ def combine(folder='/Users/tehan/Downloads/'):
     odd = ascii.read(folder + 'ListofMdwarfTICs_crossmatch_odd.csv')
     occurrences = even['occurances'].data + odd['occurances'].data
     # print(len(np.where(sector_num-occurrences != 0)[0]))
-    print(len(np.where(sector_num-occurrences > 0)[0]))
+    print(len(np.where(sector_num - occurrences > 0)[0]))
+    old_lcs = np.zeros((len(sector_num)))
+    for i in range(len(sector_num)):
+        if sector_num[i] - occurrences[i] > 0 and occurrences[i] > 0:
+            old_lcs[i] = occurrences[i]
+    print(np.sum(np.where(old_lcs > 0)[0]))
     # print(len(np.where(sector_num == 0)[0]))
     # print(even[np.where(sector_num-occurrences > 0)[0][0]], odd[np.where(sector_num-occurrences > 0)[0][0]])
     # print(len(np.where(occurrences == 0)[0]))
-    missing = all[np.where(sector_num-occurrences > 0)[0]]
-    print(sum(sector_num-occurrences))
-    missing.write(folder + 'ListofMdwarfTICs_crossmatch_missing.csv')
+    missing = all[np.where(sector_num - occurrences > 0)[0]]
+    print(sum(sector_num - occurrences))
+    # missing.write(folder + 'ListofMdwarfTICs_crossmatch_missing.csv')
+
 
 if __name__ == '__main__':
     # targets = crossmatch()
