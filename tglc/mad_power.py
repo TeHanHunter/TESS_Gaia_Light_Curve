832e2251813582a9ab035c57cd447fa2b26981d9
tehan <tehanhunter@gmail.com>
2024-06-11 14:44:52 -0700

    mismatch

--- tglc/mad_power.py
+++ tglc/mad_power.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import glob
 def get_mad(folder, power=1.4):
     files = glob.glob(f'{folder}*{power}.npy')
-    print(files)
+    # print(files)
     mag = np.array([])
     mad = np.array([])
     for file in files:
@@ -14,12 +14,19 @@ def get_mad(folder, power=1.4):
     mag = mag[idx]
     mad = mad[idx]
     aper_precision = 1.48 * mad / (np.sqrt(2) * 1.5e4 * 10 ** ((10 - mag) / 2.5))
-    print(mad)
+    print(len(mag))
     return mag, aper_precision
 
 if __name__ == '__main__':
-    mag, aper_precision = get_mad('/Users/tehan/Documents/TGLC/MAD_power/', power=1.0)
-    plt.plot(mag, aper_precision, '.')
+    mag, aper_precision = get_mad('/Users/tehan/Documents/TGLC/MAD_power/', power=1.3)
+
+    bin_size = 200
+    tglc_mag = np.median(mag[:len(mag) // bin_size * bin_size].reshape(-1, bin_size), axis=1)
+    tglc_binned = np.median(aper_precision[:len(aper_precision) // bin_size * bin_size].reshape(-1, bin_size),
+                            axis=1)
+
+    plt.plot(mag, aper_precision, '.k', alpha=0.1)
+    plt.plot(tglc_mag, tglc_binned, 'r', zorder=2)
     plt.ylim(1e-4,1)
     plt.yscale('log')
     plt.show()
