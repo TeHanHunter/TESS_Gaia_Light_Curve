57453545e14172e59232e1e99aadefc3def9a9e0
tehan <tehanhunter@gmail.com>
2024-10-22 13:37:18 -0700

    Update tutorial

--- tglc/run.py
+++ tglc/run.py
@@ -33,8 +33,10 @@ def lc_per_cut(i, local_directory=''):
     with open(f'{local_directory}source/{camccd}/source_{cut_x:02d}_{cut_y:02d}.pkl', 'rb') as input_:
         try:
             source = pickle.load(input_)
-        except:
-            print(f'{camccd}/source_{cut_x:02d}_{cut_y:02d}')
+        except EOFError:
+            print(f'{camccd}/source_{cut_x:02d}_{cut_y:02d} - EOFError')
+        except Exception as e:
+            print(f'{camccd}/source_{cut_x:02d}_{cut_y:02d} - Exception: {e}')
     epsf(source, psf_size=11, factor=2, cut_x=cut_x, cut_y=cut_y, sector=source.sector, power=1.4,
          local_directory=local_directory, limit_mag=16, save_aper=False, no_progress_bar=True)
 
