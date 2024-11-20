4942073c312af9f3518d0e70fc435e0392906db6
tehan <tehanhunter@gmail.com>
2024-11-20 09:24:36 -0800

    Radius bias

--- tglc/lc_plot.py
+++ tglc/lc_plot.py
@@ -24,6 +24,7 @@ import seaborn as sns
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 from astroquery.mast import Catalogs
+from scipy.stats import bootstrap
 
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['mathtext.fontset'] = 'dejavuserif'  # Use Computer Modern (serif font)
@@ -47,10 +48,11 @@ def read_parameter(file=None):
                            float(lines[i].split(',')[1].split('$')[2])])
     return table
 
+
 def fetch_contamrt(folder=None):
     files = glob(os.path.join(folder, '*/lc/*.fits'))
-    tic_sec=[]
-    contamrt=[]
+    tic_sec = []
+    contamrt = []
     for i in range(len(files)):
         with fits.open(files[i]) as hdul:
             tic_sec.append(f'TIC_{hdul[0].header["TICID"]}_{hdul[0].header["SECTOR"]}')
@@ -58,6 +60,7 @@ def fetch_contamrt(folder=None):
     table = Table([tic_sec, contamrt], names=('tic_sec', 'contamrt'))
     table.write(f'{folder}contamination_ratio.dat', format='ascii', overwrite=True)
     return table
+
 
 def figure_1(folder='/home/tehan/Downloads/Data/', param='pl_rade', r1=0.01, r2=0.4, cmap='Tmag', pipeline='TGLC'):
     param_dict = {'pl_rade': 'r_pl__0', 'pl_ratror': 'ror__0'}
@@ -408,7 +411,174 @@ def compute_weighted_mean_all(data):
     return difference_values, errors_ratio, weighted_mean, weighted_mean_error
 
 
+def compute_weighted_mean_bootstrap(data):
+    fit_values = data['value']
+    pl_ratror = data['pl_ratror']
+    errors_value = (data['err1'] - data['err2']) / 2
+    errors_pl_ratror = (data['pl_ratrorerr1'] - data['pl_ratrorerr2']) / 2
+
+    # correct literature with 0 error
+    for i in range(len(errors_pl_ratror)):
+        if errors_pl_ratror[i] == 0:
+            errors_pl_ratror[i] = errors_value[i]
+            # print(errors_pl_ratror[i])
+    # Compute the ratio and its propagated error
+    difference_values = fit_values - pl_ratror
+    errors_ratio = np.sqrt(errors_value ** 2 + errors_pl_ratror ** 2)
+
+    # Compute inverse variance weighted mean
+    def weighted_mean(values_, weights_):
+        return np.sum(values_ * weights_) / np.sum(weights_)
+
+    weights = 1 / (errors_ratio ** 2)
+
+    def weighted_mean_stat(values_):
+        return weighted_mean(values_, weights)
+
+    # weighted_mean = np.sum(difference_values * weights) / np.sum(weights)
+    # weighted_mean_error = np.sqrt(1 / np.sum(weights))
+    res = bootstrap((difference_values,), weighted_mean_stat, confidence_level=.95, n_resamples=10000, method='percentile')
+    iw_mean = weighted_mean(difference_values, weights)
+    ci_low, ci_high = res.confidence_interval.low, res.confidence_interval.high
+    print(f"Inverse-Variance Weighted Mean: {iw_mean}")
+    print(f"68% Confidence Interval: ({ci_low}, {ci_high})")
+    print(res.standard_error)
+    return iw_mean, ci_low+(iw_mean-ci_low)/2, iw_mean+(ci_high-iw_mean)/2
+
+
 def figure_4(folder='/home/tehan/Downloads/Data/', ):
+    palette = sns.color_palette('bright')
+    tglc_color = 'C1'
+    qlp_color = 'C0'
+    sns.set(rc={'font.family': 'serif', 'font.serif': 'DejaVu Serif', 'font.size': 12,
+                'axes.edgecolor': '0.2', 'axes.labelcolor': '0.', 'xtick.color': '0.', 'ytick.color': '0.',
+                'axes.facecolor': '0.95', 'grid.color': '0.8'})
+    fig, ax = plt.subplots(2, 1, sharex=True, figsize=(6, 8), gridspec_kw={'hspace': 0.1})
+    # ground
+    difference_qlp = ascii.read(f'{folder}deviation_QLP.dat')
+    difference_tglc = ascii.read(f'{folder}deviation_TGLC.dat')
+    d_qlp = difference_qlp[np.where(difference_qlp['rhat'] < 1.1)]
+    d_qlp['Pipeline'] = ['QLP'] * len(d_qlp)
+    # print(len(d_qlp))
+    d_tglc = difference_tglc[np.where(difference_tglc['rhat'] < 1.1)]
+    d_tglc['Pipeline'] = ['TGLC'] * len(d_tglc)
+    # print(len(d_tglc))
+    difference_qlp = Table(names=d_qlp.colnames, dtype=[col.dtype for col in d_qlp.columns.values()])
+    difference_tglc = Table(names=d_tglc.colnames, dtype=[col.dtype for col in d_tglc.columns.values()])
+    ground = [156648452, 154293917, 271893367, 285048486, 88992642, 454248975, 428787891, 394722182, 395171208,
+              445751830, 7548817, 86263325, 155867025, 198008005, 178162579, 289661991, 464300749, 151483286, 335590096,
+              17865622, 193641523, 396562848, 447061717, 124379043, 44792534, 150098860, 179317684, 124029677, 95660472,
+              395393265, 310002617, 220076110, 20182780, 70524163, 95057860, 376524552, 394050135, 409794137, 243641947,
+              419411415, 281408474, 460984940, 68007716, 39414571, 8599009, 33595516, 458419328, 336128819, 417646390,
+              240823272, 147977348, 144700903, 258920431, 280655495, 66561343, 16005254, 375506058, 279947414,
+              239816546, 361343239]
+    for i in range(len(d_tglc)):
+        star_sector = d_tglc['Star_sector'][i]
+        if star_sector in d_qlp['Star_sector']:
+            if int(star_sector.split('_')[1]) in ground:
+                difference_tglc.add_row(d_tglc[i])
+                difference_qlp.add_row(d_qlp[np.where(d_qlp['Star_sector'] == star_sector)[0][0]])
+    diff_tglc, errors_tglc, weighted_mean_tglc, weighted_mean_error_tglc = compute_weighted_mean_all(difference_tglc)
+    iw_mean_tglc, ci_low_tglc, ci_high_tglc = compute_weighted_mean_bootstrap(difference_tglc)
+    # QLP data (dim)
+    diff_qlp, errors_qlp, weighted_mean_qlp, weighted_mean_error_qlp = compute_weighted_mean_all(difference_qlp)
+    iw_mean_qlp, ci_low_qlp, ci_high_qlp = compute_weighted_mean_bootstrap(difference_qlp)
+    print(len(difference_tglc))
+    # sns.violinplot(data=df, x="diff", y="Tmag_int", hue="Pipeline", split=True, bw_adjust=.6, gap=.04, alpha=0.6,
+    #                gridsize=500, width=1.2, palette=[tglc_color, qlp_color])
+    ax[0].hist(diff_qlp, bins=np.linspace(-0.05, 0.05, 41),
+               weights=(1 / errors_qlp ** 2) * len(diff_qlp) / np.sum(1 / errors_qlp ** 2),
+               color=qlp_color, alpha=0.6, edgecolor=None)
+
+    ax[0].hist(diff_tglc, bins=np.linspace(-0.05, 0.05, 41),
+               weights=(1 / errors_tglc ** 2) * len(diff_tglc) / np.sum(1 / errors_tglc ** 2),
+               color=tglc_color, alpha=0.6, edgecolor=None)
+    ax[0].set_title('Ground-based-only radius (188 light curves)')
+    ax[0].scatter(weighted_mean_tglc, 2.6, marker='v', color=tglc_color, edgecolors='k', linewidths=0.7, s=50,
+                  zorder=3, label='TGLC')
+    ax[0].errorbar(iw_mean_tglc, 1.6, xerr=[[iw_mean_tglc-ci_low_tglc], [ci_high_tglc-iw_mean_tglc]], ecolor='k',
+                   elinewidth=1,capsize=3, zorder=2,)
+    ax[0].scatter(weighted_mean_qlp, 2.6, marker='v', color=qlp_color, edgecolors='k', linewidths=0.7, s=50,
+                  zorder=3, label='QLP')
+    ax[0].errorbar(iw_mean_qlp, 1.6, xerr=[[iw_mean_qlp-ci_low_qlp], [ci_high_qlp-iw_mean_qlp]], ecolor='k',
+                   elinewidth=1,capsize=3, zorder=2,)
+
+    ax[0].vlines(0, ymin=0, ymax=52.5, color='k', ls='dashed', lw=1, zorder=3)
+    ax[0].set_xlabel('')
+    ax[0].set_ylabel('Error Weighted Counts')
+    ax[0].legend(loc='upper right')
+    # ax[0].set_xticks([-0.06, -0.04, -0.02, 0, 0.02, 0.04, 0.06],
+    #            [r'$-6\%$', r'$-4\%$', r'$-2\%$', r'$0\%$', r'$2\%$', r'$4\%$', r'$6\%$'])
+    # plt.xlim(-0.05, 0.05)
+    # plt.ylim(-1,2)
+    # no-ground
+    difference_qlp = ascii.read(f'{folder}deviation_QLP.dat')
+    difference_tglc = ascii.read(f'{folder}deviation_TGLC.dat')
+    d_qlp = difference_qlp[np.where(difference_qlp['rhat'] < 1.1)]
+    d_qlp['Pipeline'] = ['QLP'] * len(d_qlp)
+    # print(len(d_qlp))
+    d_tglc = difference_tglc[np.where(difference_tglc['rhat'] < 1.1)]
+    d_tglc['Pipeline'] = ['TGLC'] * len(d_tglc)
+    # print(len(d_tglc))
+    difference_qlp = Table(names=d_qlp.colnames, dtype=[col.dtype for col in d_qlp.columns.values()])
+    difference_tglc = Table(names=d_tglc.colnames, dtype=[col.dtype for col in d_tglc.columns.values()])
+    no_ground = [428699140, 201248411, 172518755, 157698565, 119584412, 262530407, 219854185, 140691463, 237922465,
+                 271478281, 29857954, 198485881, 332558858, 376637093, 54002556, 126606859, 231702397, 460205581,
+                 351601843, 24358417, 144193715, 219016883, 445805961, 103633434, 230001847, 70899085, 147950620,
+                 219854519, 333657795, 200322593, 287256467, 206541859, 420112589, 261867566, 10837041, 70513361,
+                 148673433, 229510866, 321669174, 183120439, 149845414, 293954617, 256722647, 280206394, 468574941,
+                 29960110, 141488193, 106402532, 392476080, 158588995, 49428710, 410214986, 441738827, 220479565,
+                 172370679, 116483514, 350153977, 37770169, 162802770, 212957629, 393831507, 207110080, 190496853,
+                 404505029, 207141131, 439456714, 394137592, 267263253, 192790476, 300038935, 169249234, 159873822,
+                 394561119, 142394656, 318753380, 422756130, 339672028, 176956893, 348835438, 62483237, 266980320,
+                 151825527, 466206508, 288735205, 237104103, 437856897, 73540072, 229742722, 1003831, 83092282,
+                 264678534, 271971130, 204650483, 394918211, 321857016, 290348383, 436873727, 362249359, 372172128]
+    for i in range(len(d_tglc)):
+        star_sector = d_tglc['Star_sector'][i]
+        if star_sector in d_qlp['Star_sector']:
+            if int(star_sector.split('_')[1]) in no_ground:
+                difference_tglc.add_row(d_tglc[i])
+                difference_qlp.add_row(d_qlp[np.where(d_qlp['Star_sector'] == star_sector)[0][0]])
+    diff_tglc, errors_tglc, weighted_mean_tglc, weighted_mean_error_tglc = compute_weighted_mean_all(difference_tglc)
+    iw_mean_tglc, ci_low_tglc, ci_high_tglc = compute_weighted_mean_bootstrap(difference_tglc)
+    # QLP data (dim)
+    diff_qlp, errors_qlp, weighted_mean_qlp, weighted_mean_error_qlp = compute_weighted_mean_all(difference_qlp)
+    iw_mean_qlp, ci_low_qlp, ci_high_qlp = compute_weighted_mean_bootstrap(difference_qlp)
+    print(len(difference_tglc))
+    # sns.violinplot(data=df, x="diff", y="Tmag_int", hue="Pipeline", split=True, bw_adjust=.6, gap=.04, alpha=0.6,
+    #                gridsize=500, width=1.2, palette=[tglc_color, qlp_color])
+    ax[1].hist(diff_qlp, bins=np.linspace(-0.05, 0.05, 41),
+               weights=(1 / errors_qlp ** 2) * len(diff_qlp) / np.sum(1 / errors_qlp ** 2),
+               color=qlp_color, alpha=0.6, edgecolor=None)
+
+    ax[1].hist(diff_tglc, bins=np.linspace(-0.05, 0.05, 41),
+               weights=(1 / errors_tglc ** 2) * len(diff_tglc) / np.sum(1 / errors_tglc ** 2),
+               color=tglc_color, alpha=0.6, edgecolor=None)
+    ax[1].set_title('TESS and Ground-based radius (302 light curves)')
+    ax[1].scatter(weighted_mean_tglc, 6.8, marker='v', color=tglc_color, edgecolors='k', linewidths=0.7, s=50,
+                  zorder=3, label='TGLC')
+    ax[1].errorbar(iw_mean_tglc, 4, xerr=[[iw_mean_tglc-ci_low_tglc], [ci_high_tglc-iw_mean_tglc]], ecolor='k',
+                   elinewidth=1,capsize=3, zorder=2,)
+    ax[1].scatter(weighted_mean_qlp, 6.8, marker='v', color=qlp_color, edgecolors='k', linewidths=0.7, s=50,
+                  zorder=3, label='QLP')
+    ax[1].errorbar(iw_mean_qlp, 4, xerr=[[iw_mean_qlp-ci_low_qlp], [ci_high_qlp-iw_mean_qlp]], ecolor='k',
+                   elinewidth=1,capsize=3, zorder=2,)
+    ax[1].vlines(0, ymin=0, ymax=145, color='k', ls='dashed', lw=1, zorder=3)
+    ax[1].set_xlabel(r'$\Delta(R_{\text{p}}/R_*)$')
+    ax[1].set_ylabel('Error Weighted Counts')
+    ax[1].legend(loc='upper right')
+
+    # ax[1].set_xticks([-0.06, -0.04, -0.02, 0, 0.02, 0.04, 0.06],
+    #                  [r'$-6\%$', r'$-4\%$', r'$-2\%$', r'$0\%$', r'$2\%$', r'$4\%$', r'$6\%$'])
+
+    plt.xlim(-0.03, 0.03)
+
+    plt.savefig(os.path.join(folder, f'ror_ground_vs_no_ground.pdf'), bbox_inches='tight', dpi=600)
+    plt.show()
+
+
+def figure_5(folder='/home/tehan/Downloads/Data/', ):
+    contamrt = ascii.read('/Users/tehan/Documents/TGLC/contamination_ratio.dat')
     palette = sns.color_palette('bright')
     tglc_color = 'C1'
     qlp_color = 'C0'
@@ -448,23 +618,18 @@ def figure_4(folder='/home/tehan/Downloads/Data/', ):
     print("QLP Weighted Mean:", weighted_mean_qlp)
     print("QLP Weighted Mean Error:", weighted_mean_error_qlp)
     print(len(difference_tglc))
+    print(diff_tglc)
     # sns.violinplot(data=df, x="diff", y="Tmag_int", hue="Pipeline", split=True, bw_adjust=.6, gap=.04, alpha=0.6,
     #                gridsize=500, width=1.2, palette=[tglc_color, qlp_color])
-    ax[0].hist(diff_qlp, bins=np.linspace(-0.05, 0.05, 41),
-               weights=(1 / errors_qlp ** 2) * len(diff_qlp) / np.sum(1 / errors_qlp ** 2),
-               color=qlp_color, alpha=0.6, edgecolor=None)
-
-    ax[0].hist(diff_tglc, bins=np.linspace(-0.05, 0.05, 41),
-               weights=(1 / errors_tglc ** 2) * len(diff_tglc) / np.sum(1 / errors_tglc ** 2),
-               color=tglc_color, alpha=0.6, edgecolor=None)
+    diff_tglc_contam = []
+    diff_qlp_contam = []
+    for i in range(len(difference_tglc)):
+        i_tglc = np.where(np.array(contamrt['tic_sec'].tolist()) == difference_tglc['Star_sector'][i])[0][0]
+        i_qlp = np.where(np.array(contamrt['tic_sec'].tolist()) == difference_qlp['Star_sector'][i])[0][0]
+        diff_tglc_contam.append(contamrt['contamrt'].tolist()[i_tglc])
+        diff_qlp_contam.append(contamrt['contamrt'].tolist()[i_qlp])
+    ax[0].scatter(diff_tglc_contam, diff_tglc)
     ax[0].set_title('Ground-based-only radius')
-    ax[0].scatter(weighted_mean_tglc, 1.6, marker='v', color=tglc_color, edgecolors='k', linewidths=0.7, s=100, zorder=2,
-                  label='TGLC')
-    ax[0].scatter(weighted_mean_qlp, 1.6, marker='v', color=qlp_color, edgecolors='k', linewidths=0.7, s=100, zorder=2,
-                  label='QLP')
-    ax[0].vlines(0, ymin=0, ymax=55, color='k', ls='dashed', lw=1, zorder=3)
-    ax[0].set_xlabel('')
-    ax[0].set_ylabel('Error Weighted Counts')
     ax[0].legend(loc='upper right')
     # ax[0].set_xticks([-0.06, -0.04, -0.02, 0, 0.02, 0.04, 0.06],
     #            [r'$-6\%$', r'$-4\%$', r'$-2\%$', r'$0\%$', r'$2\%$', r'$4\%$', r'$6\%$'])
@@ -506,21 +671,33 @@ def figure_4(folder='/home/tehan/Downloads/Data/', ):
     print("QLP Weighted Mean:", weighted_mean_qlp)
     print("QLP Weighted Mean Error:", weighted_mean_error_qlp)
     print(len(difference_tglc))
+    diff_tglc_contam = []
+    diff_qlp_contam = []
+    for i in range(len(difference_tglc)):
+        try:
+            i_tglc = np.where(np.array(contamrt['tic_sec'].tolist()) == difference_tglc['Star_sector'][i])[0][0]
+            i_qlp = np.where(np.array(contamrt['tic_sec'].tolist()) == difference_qlp['Star_sector'][i])[0][0]
+            diff_tglc_contam.append(contamrt['contamrt'].tolist()[i_tglc])
+            diff_qlp_contam.append(contamrt['contamrt'].tolist()[i_qlp])
+        except IndexError:
+            diff_tglc_contam.append(0.)
+            diff_qlp_contam.append(0.)
+    ax[1].scatter(diff_tglc_contam, diff_tglc)
     # sns.violinplot(data=df, x="diff", y="Tmag_int", hue="Pipeline", split=True, bw_adjust=.6, gap=.04, alpha=0.6,
     #                gridsize=500, width=1.2, palette=[tglc_color, qlp_color])
-    ax[1].hist(diff_qlp, bins=np.linspace(-0.05, 0.05, 41),
-               weights=(1 / errors_qlp ** 2) * len(diff_qlp) / np.sum(1 / errors_qlp ** 2),
-               color=qlp_color, alpha=0.6, edgecolor=None)
-
-    ax[1].hist(diff_tglc, bins=np.linspace(-0.05, 0.05, 41),
-               weights=(1 / errors_tglc ** 2) * len(diff_tglc) / np.sum(1 / errors_tglc ** 2),
-               color=tglc_color, alpha=0.6, edgecolor=None)
-    ax[1].set_title('TESS and Ground-based radius')
-    ax[1].scatter(weighted_mean_tglc, 4, marker='v', color=tglc_color, edgecolors='k', linewidths=0.7, s=100, zorder=2,
-                  label='TGLC')
-    ax[1].scatter(weighted_mean_qlp, 4, marker='v', color=qlp_color, edgecolors='k', linewidths=0.7, s=100, zorder=2,
-                  label='QLP')
-    ax[1].vlines(0, ymin=0, ymax=145, color='k', ls='dashed', lw=1, zorder=3)
+    # ax[1].hist(diff_qlp, bins=np.linspace(-0.05, 0.05, 41),
+    #            weights=(1 / errors_qlp ** 2) * len(diff_qlp) / np.sum(1 / errors_qlp ** 2),
+    #            color=qlp_color, alpha=0.6, edgecolor=None)
+    #
+    # ax[1].hist(diff_tglc, bins=np.linspace(-0.05, 0.05, 41),
+    #            weights=(1 / errors_tglc ** 2) * len(diff_tglc) / np.sum(1 / errors_tglc ** 2),
+    #            color=tglc_color, alpha=0.6, edgecolor=None)
+    # ax[1].set_title('TESS and Ground-based radius')
+    # ax[1].scatter(weighted_mean_tglc, 4, marker='v', color=tglc_color, edgecolors='k', linewidths=0.7, s=100, zorder=2,
+    #               label='TGLC')
+    # ax[1].scatter(weighted_mean_qlp, 4, marker='v', color=qlp_color, edgecolors='k', linewidths=0.7, s=100, zorder=2,
+    #               label='QLP')
+    # ax[1].vlines(0, ymin=0, ymax=145, color='k', ls='dashed', lw=1, zorder=3)
     ax[1].set_xlabel(r'$\Delta(R_{\text{p}}/R_*)$')
     ax[1].set_ylabel('Error Weighted Counts')
     ax[1].legend(loc='upper right')
@@ -528,13 +705,16 @@ def figure_4(folder='/home/tehan/Downloads/Data/', ):
     # ax[1].set_xticks([-0.06, -0.04, -0.02, 0, 0.02, 0.04, 0.06],
     #                  [r'$-6\%$', r'$-4\%$', r'$-2\%$', r'$0\%$', r'$2\%$', r'$4\%$', r'$6\%$'])
 
-    plt.xlim(-0.03, 0.03)
-
-    plt.savefig(os.path.join(folder, f'ror_ground_vs_no_ground.pdf'), bbox_inches='tight', dpi=600)
+    # plt.xlim(-0.03, 1)
+    ax[0].set_ylim(-0.05, 0.05)
+    ax[1].set_ylim(-0.05, 0.05)
+    ax[0].set_xscale('log')
+    ax[1].set_xscale('log')
+    plt.savefig(os.path.join(folder, f'ror_contamrt.pdf'), bbox_inches='tight', dpi=600)
     plt.show()
 
 
-def figure_5(folder='/home/tehan/Downloads/Data/', param='pl_rade', r1=0.0001, r2=0.16, cmap='Tmag'):
+def figure_6(folder='/home/tehan/Downloads/Data/', param='pl_rade', r1=0.0001, r2=0.16, cmap='Tmag'):
     param_dict = {'pl_rade': 'r_pl__0', 'pl_ratror': 'ror__0'}
     t = ascii.read(pkg_resources.resource_stream(__name__, 'PSCompPars_2024.02.05_22.52.50.csv'))
     # t = ascii.read('/home/tehan/PycharmProjects/TESS_Gaia_Light_Curve/tglc/PSCompPars_2024.02.05_22.52.50.csv')
@@ -616,7 +796,7 @@ def figure_5(folder='/home/tehan/Downloads/Data/', param='pl_rade', r1=0.0001, r
     plt.savefig(os.path.join(folder, f'{param}_diagonal_transit_depth.png'), bbox_inches='tight', dpi=600)
 
 
-def figure_6(type='all'):
+def figure_7(type='all'):
     palette = sns.color_palette('colorblind')
     sns.set(rc={'font.family': 'serif', 'font.serif': 'DejaVu Serif', 'font.size': 12,
                 'axes.edgecolor': '0.2', 'axes.labelcolor': '0.', 'xtick.color': '0.', 'ytick.color': '0.',
@@ -658,7 +838,7 @@ def figure_6(type='all'):
         plt.show()
 
 
-def figure_7(type='all'):
+def figure_8(type='all'):
     palette = sns.color_palette('colorblind')
     sns.set(rc={'font.family': 'serif', 'font.serif': 'DejaVu Serif', 'font.size': 12,
                 'axes.edgecolor': '0.2', 'axes.labelcolor': '0.', 'xtick.color': '0.', 'ytick.color': '0.',
@@ -708,6 +888,6 @@ def figure_7(type='all'):
 
 if __name__ == '__main__':
     # figure_1(folder='/home/tehan/Downloads/Data_qlp/', r1=0.01, param='pl_ratror', cmap='Tmag', pipeline='QLP')
-    fetch_contamrt(folder='/home/tehan/data/cosmos/transit_depth_validation_contamrt/')
-    # figure_3(folder='/Users/tehan/Documents/TGLC/')
+    # fetch_contamrt(folder='/home/tehan/data/cosmos/transit_depth_validation_contamrt/')
+    figure_4(folder='/Users/tehan/Documents/TGLC/')
     # figure_5(type='phase-fold')
