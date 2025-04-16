247c8332b4e456b903512b97c07774058da64aca
Jack Haviland <haviland@mit.edu>
2025-04-16 16:54:52 -0400

    Fixes for 5x5 logic near edges

--- tglc/target_lightcurve.py
+++ tglc/target_lightcurve.py
@@ -359,7 +359,7 @@ def epsf(source, psf_size=11, factor=2, local_directory='', target=None, cut_x=0
                               bg=background_, time=source.time, psf_lc=psf_lc, cal_psf_lc=cal_psf_lc, aper_lc=aper_lc,
                               cal_aper_lc=cal_aper_lc, local_bg=local_bg, x_aperture=x_aperture[i],
                               y_aperture=y_aperture[i], near_edge=near_edge, save_aper=save_aper, portion=portion,
-                              prior=prior)
+                              prior=prior, transient=source.transient, target_5x5=target_5x5, field_stars_5x5=field_stars_5x5)
                 else:
                     lc_output(source, local_directory=lc_directory, index=i,
                               tess_flag=source.quality, cut_x=cut_x, cut_y=cut_y, cadence=source.cadence,
