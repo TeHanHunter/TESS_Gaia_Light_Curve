25a35b5b18e69976815806d5fa1855e7294add9e
Te Han <tehanhunter@gmail.com>
2025-07-03 11:43:26 -0700

    contam check

--- tglc/quick_lc.py
+++ tglc/quick_lc.py
@@ -810,24 +810,24 @@ def get_tglc_lc(tics=None, sectors=None, method='query', server=1, directory=Non
 
 
 if __name__ == '__main__':
-    tics = [86263325]
-    sectors = [49]
+    tics = [371880799, 318968532]
+    sectors = [None] * 2
     directory = f'/Users/tehan/Downloads/'
     # directory = '/home/tehan/data/cosmos/GEMS/'
     os.makedirs(directory, exist_ok=True)
-    # get_tglc_lc(tics=tics, sectors=sectors, method='query', server=1, directory=directory)
+    get_tglc_lc(tics=tics, sectors=sectors, method='query', server=1, directory=directory)
 
     # plot_lc(local_directory=f'{directory}TIC {tics[0]}/', kind='cal_aper_flux')
     # plot_lc(local_directory=f'/home/tehan/Documents/tglc/TIC 16005254/', kind='cal_aper_flux', ylow=0.9, yhigh=1.1)
-    # for i in range(len(tics)):
-    #     plot_contamination(local_directory=f'{directory}TIC {tics[i]}/', gaia_dr3=None)
-    #     print('done')
+    for i in range(len(tics)):
+        plot_contamination(local_directory=f'{directory}TIC {tics[i]}/', gaia_dr3=None)
+        print('done')
     # plot_contamination(local_directory=f'{directory}TIC {tics[0]}/', gaia_dr3=4597001770059110528)
     # plot_epsf(local_directory=f'{directory}TIC {tics[0]}/')
     # plot_pf_lc_points(local_directory=f'{directory}TIC {tics[0]}/lc/', period=3.792622, mid_transit_tbjd=2459477.3131,
     #            kind='cal_aper_flux')
-    plot_pf_lc(local_directory=f'{directory}TIC {tics[0]}/lc/', period=11, mid_transit_tbjd=1738.71248,
-               kind='aperture_flux')
+    # plot_pf_lc(local_directory=f'{directory}TIC {tics[0]}/lc/', period=11, mid_transit_tbjd=1738.71248,
+    #            kind='aperture_flux')
 
     # directory='/home/tehan/data/cosmos/transit_depth_validation_contamrt/'
     # tic_sector = ascii.read(glob(f'{directory}deviation_TGLC_extra.dat')[0])
