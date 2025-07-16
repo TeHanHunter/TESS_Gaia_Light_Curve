35b079947924058665e187562a8f20ac0d07cb68
Te Han <tehanhunter@gmail.com>
2025-07-16 13:39:35 -0700

    contam check

---
+++ file_name_copy.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from pathlib import Path
+import shutil
+
+# ---- CONFIGURATION ----
+csv_path = "/home/tehan/data/cosmos/Oddo_2025/Jul2025_new_EBs_tesspoint_output_filtered.csv"
+destination = Path("/home/tehan/data/cosmos/Oddo_2025/")
+copy_group = "odd"  # options: "odd", "even", "both"
+# --- LOAD CSV ---
+df = pd.read_csv(csv_path, sep="\t")
+
+# --- COPY LOOP ---
+for _, row in df.iterrows():
+    outsec = int(row["outSec"])
+    if copy_group == "odd" and outsec % 2 == 0:
+        continue
+    if copy_group == "even" and outsec % 2 != 0:
+        continue
+
+    gaia_id = row["GAIA DR3"].split()[-1]
+    outcam = row["outCam"]
+    outccd = row["outCcd"]
+
+    src_path = Path(
+        f"/home/tehan/data/sector{outsec:04d}/lc/"
+        f"hlsp_tglc_tess_ffi_gaiaid-{gaia_id}-s{outsec:04d}-"
+        f"cam{outcam}-ccd{outccd}_tess_v1_llc.fits"
+    )
+
+    if src_path.exists():
+        print(f"Copying {src_path.name}")
+        shutil.copy2(src_path, destination)
+    else:
+        print(f"Missing: {src_path}")
