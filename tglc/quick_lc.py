302703fdf934bb867692dbe1f82be181fcb1fab5
Te Han <tehanhunter@gmail.com>
2025-08-15 12:49:18 -0700

    200pc

--- tglc/quick_lc.py
+++ tglc/quick_lc.py
@@ -821,7 +821,7 @@ def get_tglc_lc(tics=None, sectors=None, method='query', server=1, directory=Non
 
 if __name__ == '__main__':
     tics = [124988112, 160328682, 168697915, 184892124, 33521996, 389723320, 443369596, 445959176, 455947620, 46432937, 95057860, 95112238]
-    sectors = [17, 32, 31, 45, 32, 28, 33, 45, 17, 32, 33, 33]
+    sectors = [7, 32, 31, 45, 32, 28, 33, 45, 17, 32, 33, 33]
     # directory = f'/Users/tehan/Downloads/'
     directory = '/home/tehan/data/cosmos/GEMS_200pc/'
     os.makedirs(directory, exist_ok=True)
